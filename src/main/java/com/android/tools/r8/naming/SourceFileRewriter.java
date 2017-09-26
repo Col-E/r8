951d5644d840374c99e118c900a7bfcec61b46c9
Jinseong Jeon <jsjeon@google.com>
2017-09-26 08:10:26 +0000

    Revert "Fix NPE in SourceFileRewriter: code could be removed entirely."
    This reverts commit 03af4092b5f4e4ade2a0b846cc7d42df6bdaeafd.
    
    Reason for revert:
    Its predecessor will be reverted, so this should be reverted ahead.
    
    Change-Id: I1a709ff6e39075db0e0e7db1826bcc525ecd77fc

--- src/main/java/com/android/tools/r8/naming/SourceFileRewriter.java
+++ src/main/java/com/android/tools/r8/naming/SourceFileRewriter.java
@@ -48,7 +48,7 @@ public class SourceFileRewriter {
         }
         Code code = encodedMethod.getCode();
         // Other kinds of {@link Code} do not have debug_info_item.
-        if (code == null || !code.isDexCode()) {
+        if (!code.isDexCode()) {
           return;
         }
         if (code.asDexCode().getDebugInfo() == null) {
