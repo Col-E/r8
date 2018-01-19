ebd34adb53c9ee7646e97cbbd6bcc9914a09fe8c
Mads Ager <ager@google.com>
2018-01-19 10:00:10 +0100

    Revert "Revert "Canonicalize constants""
    This reverts commit 09dcf6cc7fd3f8c86a953251c66311d31e6c122b.
    
    R=mikaelpeltier@google.com, sgjesse@google.com
    
    Change-Id: I57ecac7da728b479ae321ad2e448ff34bcd1a968

--- src/main/java/com/android/tools/r8/ir/conversion/IRConverter.java
+++ src/main/java/com/android/tools/r8/ir/conversion/IRConverter.java
@@ -27,6 +27,7 @@ import com.android.tools.r8.ir.desugar.InterfaceMethodRewriter;
 import com.android.tools.r8.ir.desugar.LambdaRewriter;
 import com.android.tools.r8.ir.desugar.StringConcatRewriter;
 import com.android.tools.r8.ir.optimize.CodeRewriter;
+import com.android.tools.r8.ir.optimize.ConstantCanonicalizer;
 import com.android.tools.r8.ir.optimize.DeadCodeRemover;
 import com.android.tools.r8.ir.optimize.Inliner;
 import com.android.tools.r8.ir.optimize.Inliner.Constraint;
@@ -601,6 +602,7 @@ public class IRConverter {
       assert code.isConsistentSSA();
     }
 
+    ConstantCanonicalizer.canonicalize(code);
     codeRewriter.useDedicatedConstantForLitInstruction(code);
     codeRewriter.shortenLiveRanges(code);
     codeRewriter.identifyReturnsArgument(method, code, feedback);
