03463d31decac4a9f43b1e91f8567114786158a8
Rico Wind <ricow@google.com>
2018-01-22 10:44:00 +0100

    Add --version flag to update_prebuilds_in_android.py
    To be used for archived versions (instead of the hash that we use for master branch releases)
    
    Change-Id: Ie71602ec0bec9e9e4c61cb00e956da1d99ca7122

--- tools/update_prebuilds_in_android.py
+++ tools/update_prebuilds_in_android.py
@@ -12,7 +12,9 @@ import tempfile
 import utils
 import urllib
 
-MASTER_BUILD_ROOT = "http://storage.googleapis.com/r8-releases/raw/master/"
+BUILD_ROOT = "http://storage.googleapis.com/r8-releases/raw/"
+MASTER_BUILD_ROOT = "%smaster/" % BUILD_ROOT
+
 JAR_TARGETS = [utils.D8, utils.R8, utils.COMPATDX, utils.COMPATPROGUARD]
 OTHER_TARGETS = ["LICENSE"]
 
@@ -22,6 +24,7 @@ def parse_arguments():
   parser.add_argument('android_root', nargs=1,
       help='Android checkout root.')
   parser.add_argument('--commit_hash', default=None, help='Commit hash')
+  parser.add_argument('--version', default=None, help='The version to download')
   return parser.parse_args()
 
 def copy_targets(root, target_root, srcs, dests):
@@ -39,8 +42,15 @@ def copy_jar_targets(root, target_root):
 def copy_other_targets(root, target_root):
   copy_targets(root, target_root, OTHER_TARGETS, OTHER_TARGETS)
 
-def download_target(root, commit_hash, target):
+def download_hash(root, commit_hash, target):
   url = MASTER_BUILD_ROOT + commit_hash + '/' + target
+  download_target(root, url, target)
+
+def download_version(root, version, target):
+  url = BUILD_ROOT + version + '/' + target
+  download_target(root, url, target)
+
+def download_target(root, url, target):
   download_path = os.path.join(root, target)
   print 'Downloading: ' + url + ' -> ' + download_path
   urllib.urlretrieve(url, download_path)
@@ -48,15 +58,20 @@ def download_target(root, commit_hash, target):
 def Main():
   args = parse_arguments()
   target_root = args.android_root[0]
-  if args.commit_hash == None:
+  if args.commit_hash == None and args.version == None:
     gradle.RunGradle(JAR_TARGETS)
     copy_jar_targets(utils.LIBS, target_root)
     copy_other_targets(utils.GENERATED_LICENSE_DIR, target_root)
   else:
+    assert args.commit_hash == None or args.version == None
     targets = map((lambda t: t + '.jar'), JAR_TARGETS) + OTHER_TARGETS
     with utils.TempDir() as root:
       for target in targets:
-        download_target(root, args.commit_hash, target)
+        if args.commit_hash:
+          download_hash(root, args.commit_hash, target)
+        else:
+          assert args.version
+          download_version(root, args.version, target)
       copy_jar_targets(root, target_root)
       copy_other_targets(root, target_root)
 
