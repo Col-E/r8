d07f75d02a1e7380f7fb4e12cb5e88a1a2598845
Søren Gjesse <sgjesse@google.com>
2018-01-23 09:13:02 +0100

    Update patch version for release
    New version is v1.1.2-dev
    
    Change-Id: I5af684005e0b0d4d6c73ea3a075be01a74679f00

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "v1.1.1-dev";
+  public static final String LABEL = "v1.1.2-dev";
 
   private Version() {
   }
