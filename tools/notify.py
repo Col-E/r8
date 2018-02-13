10307de0dc23097af79a3b0e721f8fc8907569c8
Jinseong Jeon <jsjeon@google.com>
2018-02-12 17:10:58 -0800

    Specify version of Notify.
    The lack of the version specification results in the following warning
    on the local run:
    /path/to/r8/tools/notify.py:8: PyGIWarning: Notify was imported without
    specifying a version first.
    
    Change-Id: I19a58f4d7d1e49a6fb31a388fd2c5662ae28326f

--- tools/notify.py
+++ tools/notify.py
@@ -5,12 +5,13 @@
 
 try:
   import gi
+  gi.require_version('Notify', '0.7')
   from gi.repository import Notify
-  Notify.init("R8 build tools")
+  Notify.init('R8 build tools')
 
   def notify(message):
     try:
-      Notify.Notification.new("R8 build tools", message).show()
+      Notify.Notification.new('R8 build tools', message).show()
     except:
       return
 
