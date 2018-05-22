c511e94251180ca93e4fc049e51938820d468863
Rico Wind <ricow@google.com>
2018-05-22 10:46:31 +0200

    Add golem build utility
    Allows update to the build targets without multi repo commits
    
    R=ager@google.com
    
    Change-Id: I5b97c06b9577f2e9ecbb823e59e573ca0dc2d61f

---
+++ tools/golem_build.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Copyright (c) 2018, the R8 project authors. Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+# Utility script to make it easier to update what golem builds.
+
+import gradle
+import sys
+
+GRADLE_ARGS = ['--no-daemon']
+BUILD_TARGETS = ['R8', 'D8', 'buildExampleJars', 'CompatDx',
+                 'downloadAndroidCts', 'downloadDx']
+
+def Main():
+  gradle.RunGradle(GRADLE_ARGS + BUILD_TARGETS)
+
+if __name__ == '__main__':
+  sys.exit(Main())
