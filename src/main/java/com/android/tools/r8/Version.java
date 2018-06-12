9d0ae2e3fc8984aafce3eaaeeaf4d18bc7d4fa10
Rico Wind <ricow@google.com>
2018-06-12 09:55:25 +0200

    Update patch version for release - new version is 1.2.24-dev
    Change-Id: I92b9fc42ae67f170062c61f1b38aa43b16fcc8cc

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "1.2.23-dev";
+  public static final String LABEL = "1.2.24-dev";
 
   private Version() {
   }
