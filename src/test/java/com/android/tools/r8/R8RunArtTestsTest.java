Col-E/r8

commit b7cb52c6c330a7d232a68220121f7cd74498f912

Support kotlin k-style lambdas in class inliner
Extending class inliner to support k-style lambdas. The only significant
change is that now classes not directly extending java.lang.Object can be
considered eligible for inlining.

This is achieved by very restrictive rules for classes not directly
inherited from java.lang.Object, which is still wide enough to allow
k-style lambdas to become eligible.

This CL also includes following improvements:

 -- hints for kotlin method parameters suggesting they are not-null and,
    thus, checked inside the method for nullability; those checks will be
    removed after inlining if we know that we pass non-null argument.
    Note that these hints don't affect semantics and only used for
    inlining heuristics (see b/72972159)

 -- based on previous work we can now mark *some* singleton instance
    field reads as always returning non-null value

 -- fix propagation of never null flag via values. This is a temp
    solution I needed to make some things work, and AFAIK Jinseong
    is going to improve nullability handling soon

GMSCORE (no time impact):
  w/o: 28,543,136
    w: 28,537,188 (-6K)

Bug: 80135269
Bug: 72972159
Change-Id: Ifcae9fe13cefaea7a10a7abd3d80983dd090b505

src/main/java/com/android/tools/r8/graph/DexEncodedMethod.java
@@ -47,6 +47,7 @@ import com.android.tools.r8.origin.Origin;
 import com.android.tools.r8.utils.InternalOptions;
 import com.google.common.collect.ImmutableList;
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
@@ -677,6 +678,7 @@ public class DexEncodedMethod extends KeyedDexItem<DexMethod> implements Resolut
     private ClassInlinerEligibility classInlinerEligibility = null;
     private TrivialInitializer trivialInitializerInfo = null;
     private ParameterUsagesInfo parametersUsages = null;
+    private BitSet kotlinNotNullParamHints = null;
 
     private OptimizationInfo() {
       // Intentionally left empty.
@@ -698,6 +700,14 @@ public class DexEncodedMethod extends KeyedDexItem<DexMethod> implements Resolut
 
     public ParameterUsage getParameterUsages(int parameter) {
       return parametersUsages == null ? null : parametersUsages.getParameterUsage(parameter);
+    }
+
+    public BitSet getKotlinNotNullParamHints() {
+      return kotlinNotNullParamHints;
+    }
+
+    public void setKotlinNotNullParamHints(BitSet hints) {
+      this.kotlinNotNullParamHints = hints;
     }
 
     public boolean returnsArgument() {
@@ -841,6 +851,10 @@ public class DexEncodedMethod extends KeyedDexItem<DexMethod> implements Resolut
 
   synchronized public void setParameterUsages(ParameterUsagesInfo parametersUsages) {
     ensureMutableOI().setParameterUsages(parametersUsages);
+  }
+
+  synchronized public void setKotlinNotNullParamHints(BitSet hints) {
+    ensureMutableOI().setKotlinNotNullParamHints(hints);
   }
 
   synchronized public void setTrivialInitializer(TrivialInitializer info) {

src/main/java/com/android/tools/r8/ir/code/CheckCast.java
@@ -35,6 +35,11 @@ public class CheckCast extends Instruction {
     this.type = type;
   }
 
+  @Override
+  boolean computeNeverNull() {
+    return object().isNeverNull();
+  }
+
   public DexType getType() {
     return type;
   }

src/main/java/com/android/tools/r8/ir/code/Instruction.java
@@ -68,6 +68,12 @@ public abstract class Instruction {
     this.position = position;
   }
 
+  boolean computeNeverNull() {
+    // By default just return the flag from the out value since it never changes.
+    assert outValue != null;
+    return outValue.isNeverNull();
+  }
+
   public final void forceSetPosition(Position position) {
     this.position = position;
   }

src/main/java/com/android/tools/r8/ir/code/Move.java
@@ -29,6 +29,11 @@ public class Move extends Instruction {
     }
   }
 
+  @Override
+  boolean computeNeverNull() {
+    return src().isNeverNull();
+  }
+
   public Value dest() {
     return outValue;
   }

src/main/java/com/android/tools/r8/ir/code/Phi.java
@@ -57,7 +57,6 @@ public class Phi extends Value {
     // period of time to break cycles. When the cycle has been resolved they are completed
     // exactly once by adding the operands.
     assert operands.isEmpty();
-    boolean canBeNull = false;
     if (block.getPredecessors().size() == 0) {
       throwUndefinedValueError();
     }
@@ -66,13 +65,10 @@ public class Phi extends Value {
       // Since this read has been delayed we must provide the local info for the value.
       Value operand = builder.readRegister(register, type, pred, edgeType, getLocalInfo());
       operand.constrainType(type);
-      canBeNull |= operand.canBeNull();
       appendOperand(operand);
     }
-    if (!canBeNull) {
-      markNeverNull();
-    }
     removeTrivialPhi();
+    recomputeNeverNull();
   }
 
   public void addOperands(List<Value> operands) {
@@ -84,19 +80,23 @@ public class Phi extends Value {
     // period of time to break cycles. When the cycle has been resolved they are completed
     // exactly once by adding the operands.
     assert this.operands.isEmpty();
-    boolean canBeNull = false;
     if (operands.size() == 0) {
       throwUndefinedValueError();
     }
     for (Value operand : operands) {
-      canBeNull |= operand.canBeNull();
       appendOperand(operand);
-    }
-    if (!canBeNull) {
-      markNeverNull();
     }
     if (removeTrivialPhi) {
       removeTrivialPhi();
+    }
+    recomputeNeverNull();
+  }
+
+  // Implementation assumes that canBeNull may change to neverNull, but
+  // not other way around. This will need to be revised later.
+  void recomputeNeverNull() {
+    if (canBeNull() && operands.stream().allMatch(Value::isNeverNull)) {
+      markNeverNull();
     }
   }
 
@@ -131,7 +131,10 @@ public class Phi extends Value {
 
   public void removeOperand(int index) {
     operands.get(index).removePhiUser(this);
-    operands.remove(index);
+    Value value = operands.remove(index);
+    if (value.canBeNull()) {
+      recomputeNeverNull();
+    }
   }
 
   public void removeOperandsByIndex(List<Integer> operandsToRemove) {
@@ -147,6 +150,7 @@ public class Phi extends Value {
       current = i + 1;
     }
     operands.addAll(copy.subList(current, copy.size()));
+    recomputeNeverNull();
   }
 
   public void replaceOperandAt(int predIndex, Value newValue) {
@@ -154,6 +158,9 @@ public class Phi extends Value {
     operands.set(predIndex, newValue);
     newValue.addPhiUser(this);
     current.removePhiUser(this);
+    if (current.canBeNull() && newValue.isNeverNull()) {
+      recomputeNeverNull();
+    }
   }
 
   void replaceOperand(Value current, Value newValue) {
@@ -162,6 +169,9 @@ public class Phi extends Value {
         operands.set(i, newValue);
         newValue.addPhiUser(this);
       }
+    }
+    if (current.canBeNull() && newValue.isNeverNull()) {
+      recomputeNeverNull();
     }
   }
 

src/main/java/com/android/tools/r8/ir/code/Value.java
@@ -628,6 +628,17 @@ public class Value {
   public void markNeverNull() {
     assert !neverNull;
     neverNull = true;
+
+    // Propagate never null flag change.
+    for (Instruction user : users) {
+      Value value = user.outValue;
+      if (value != null && value.canBeNull() && user.computeNeverNull()) {
+        value.markNeverNull();
+      }
+    }
+    for (Phi phi : phiUsers) {
+      phi.recomputeNeverNull();
+    }
   }
 
   /**

src/main/java/com/android/tools/r8/ir/conversion/IRConverter.java
@@ -65,6 +65,7 @@ import com.google.common.base.Suppliers;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -630,6 +631,11 @@ public class IRConverter {
     printC1VisualizerHeader(method);
     printMethod(code, "Initial IR (SSA)");
 
+    if (method.getCode() != null && method.getCode().isJarCode() &&
+        appInfo.definitionFor(method.method.holder).hasKotlinInfo()) {
+      computeKotlinNotNullParamHints(feedback, method, code);
+    }
+
     if (options.canHaveArtStringNewInitBug()) {
       CodeRewriter.ensureDirectStringNewToInit(code);
     }
@@ -655,7 +661,8 @@ public class IRConverter {
     }
 
     if (memberValuePropagation != null) {
-      memberValuePropagation.rewriteWithConstantValues(code, method.method.holder);
+      memberValuePropagation.rewriteWithConstantValues(
+          code, method.method.holder, isProcessedConcurrently);
     }
     if (options.enableSwitchMapRemoval && appInfo.hasLiveness()) {
       codeRewriter.removeSwitchMaps(code);
@@ -777,6 +784,36 @@ public class IRConverter {
 
     printMethod(code, "Optimized IR (SSA)");
     finalizeIR(method, code, feedback);
+  }
+
+  private void computeKotlinNotNullParamHints(
+      OptimizationFeedback feedback, DexEncodedMethod method, IRCode code) {
+    // Try to infer Kotlin non-null parameter check to use it as a hint.
+    List<Value> arguments = code.collectArguments(true);
+    BitSet paramsCheckedForNull = new BitSet();
+    DexMethod checkParameterIsNotNull =
+        code.options.itemFactory.kotlin.intrinsics.checkParameterIsNotNull;
+    for (int index = 0; index < arguments.size(); index++) {
+      Value argument = arguments.get(index);
+      for (Instruction user : argument.uniqueUsers()) {
+        // To enforce parameter non-null requirement Kotlin uses intrinsic:
+        //    kotlin.jvm.internal.Intrinsics.checkParameterIsNotNull(param, message)
+        //
+        // with the following we simply look if the parameter is ever passed
+        // to the mentioned intrinsic as the first argument. We do it only for
+        // code coming from Java classfile, since after the method is rewritten
+        // by R8 this call gets inlined.
+        if (!user.isInvokeStatic() ||
+            user.asInvokeMethod().getInvokedMethod() != checkParameterIsNotNull ||
+            user.inValues().indexOf(argument) != 0) {
+          continue;
+        }
+        paramsCheckedForNull.set(index);
+      }
+    }
+    if (paramsCheckedForNull.length() > 0) {
+      feedback.setKotlinNotNullParamHints(method, paramsCheckedForNull);
+    }
   }
 
   private void finalizeIR(DexEncodedMethod method, IRCode code, OptimizationFeedback feedback) {

src/main/java/com/android/tools/r8/ir/conversion/OptimizationFeedback.java
@@ -9,6 +9,7 @@ import com.android.tools.r8.graph.DexEncodedMethod.ClassInlinerEligibility;
 import com.android.tools.r8.graph.DexEncodedMethod.ParameterUsagesInfo;
 import com.android.tools.r8.graph.DexEncodedMethod.TrivialInitializer;
 import com.android.tools.r8.ir.optimize.Inliner.Constraint;
+import java.util.BitSet;
 
 public interface OptimizationFeedback {
   void methodReturnsArgument(DexEncodedMethod method, int argument);
@@ -21,4 +22,5 @@ public interface OptimizationFeedback {
   void setClassInlinerEligibility(DexEncodedMethod method, ClassInlinerEligibility eligibility);
   void setTrivialInitializer(DexEncodedMethod method, TrivialInitializer info);
   void setParameterUsages(DexEncodedMethod method, ParameterUsagesInfo parameterUsagesInfo);
+  void setKotlinNotNullParamHints(DexEncodedMethod method, BitSet hints);
 }

commit 81fb23949b0c2791b87e6e235b4d4a20eba598a1

Fix for build failure introduced in b7cb52c6c330
Bug:
Change-Id: Id6c1a44a85cd0d4568d386e1b6bab757f588fc08

src/test/java/com/android/tools/r8/R8RunArtTestsTest.java
@@ -1009,6 +1009,11 @@ public abstract class R8RunArtTestsTest {
   );
 
   private static List<String> requireClassInliningToBeDisabled = ImmutableList.of(
+      // Test is registered to be failing (failingRunWithArt), it fails only on 4.0.4
+      // (because of a bug in this version of dalvik) but succeeds on later versions.
+      // If class inliner us enabled it actually works fine on 4.0.4 since the class
+      // instantiation is properly inlined.
+      "301-abstract-protected",
       // Test depends on exception produced for missing method or similar cases, but
       // after class inlining removes class instantiations and references the exception
       // is not produced.