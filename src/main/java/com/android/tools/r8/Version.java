d3b22e2dd614c7ff99b122ddd5cb3239707ebb28
Christoffer Quist Adamsen <christofferqa@google.com>
2018-07-09 08:47:05 +0200

    Update patch version for release - new version is 1.3.6-dev
    Change-Id: I3b56d9348e42126dc21275f7ca941573376b6c5b

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "1.3.5-dev";
+  public static final String LABEL = "1.3.6-dev";
 
   private Version() {
   }
