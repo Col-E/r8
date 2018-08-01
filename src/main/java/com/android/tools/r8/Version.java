da4d7a3b1b2650f6339b28a50fe37e13cd05dd57
Christoffer Quist Adamsen <christofferqa@google.com>
2018-08-01 10:36:17 +0200

    Update patch version for release - new version is 1.3.13-dev
    Change-Id: Ibec7aaa2c545062792d3cff0e13ee61a6feb242e

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "1.3.12-dev";
+  public static final String LABEL = "1.3.13-dev";
 
   private Version() {
   }
