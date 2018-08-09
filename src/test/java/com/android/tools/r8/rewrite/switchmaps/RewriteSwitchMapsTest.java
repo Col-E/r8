26ac405c512a77252e87bca1f06493354f37b0a3
Tamas Kenez <tamaskenez@google.com>
2018-08-09 15:44:54 +0200

    Enable CF on RewriteSwitchMapsTest.
    Change-Id: I58c7c00ed214e0bf3e9ce42de6bc64c01b0c5baa

--- src/test/java/com/android/tools/r8/rewrite/switchmaps/RewriteSwitchMapsTest.java
+++ src/test/java/com/android/tools/r8/rewrite/switchmaps/RewriteSwitchMapsTest.java
@@ -4,7 +4,6 @@
 package com.android.tools.r8.rewrite.switchmaps;
 
 import com.android.tools.r8.CompilationMode;
-import com.android.tools.r8.DexIndexedConsumer;
 import com.android.tools.r8.R8Command;
 import com.android.tools.r8.TestBase;
 import com.android.tools.r8.ToolHelper;
@@ -13,11 +12,27 @@ import com.android.tools.r8.utils.AndroidApp;
 import com.android.tools.r8.utils.codeinspector.CodeInspector;
 import com.google.common.collect.ImmutableList;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class RewriteSwitchMapsTest extends TestBase {
+
+  private Backend backend;
+
+  @Parameterized.Parameters(name = "Backend: {0}")
+  public static Collection<Backend> data() {
+    return Arrays.asList(Backend.values());
+  }
+
+  public RewriteSwitchMapsTest(Backend backend) {
+    this.backend = backend;
+  }
 
   private static final String JAR_FILE = "switchmaps.jar";
   private static final String SWITCHMAP_CLASS_NAME = "switchmaps.Switches$1";
@@ -37,15 +52,16 @@ public class RewriteSwitchMapsTest extends TestBase {
   }
 
   private void run(CompilationMode compilationMode) throws Exception {
-    R8Command command = R8Command.builder()
-        .addProgramFiles(Paths.get(ToolHelper.EXAMPLES_BUILD_DIR).resolve(JAR_FILE))
-        .addLibraryFiles(ToolHelper.getDefaultAndroidJar())
-        .addProguardConfiguration(PG_CONFIG, Origin.unknown())
-        .setMode(compilationMode)
-        .setProgramConsumer(DexIndexedConsumer.emptyConsumer())
-        .build();
+    R8Command command =
+        R8Command.builder()
+            .addProgramFiles(Paths.get(ToolHelper.EXAMPLES_BUILD_DIR).resolve(JAR_FILE))
+            .addLibraryFiles(runtimeJar(backend))
+            .addProguardConfiguration(PG_CONFIG, Origin.unknown())
+            .setMode(compilationMode)
+            .setProgramConsumer(emptyConsumer(backend))
+            .build();
     AndroidApp result = ToolHelper.runR8(command);
-    CodeInspector inspector = new CodeInspector(result);
+    CodeInspector inspector = new CodeInspector(result, o -> o.enableCfFrontend = true);
     Assert.assertFalse(inspector.clazz(SWITCHMAP_CLASS_NAME).isPresent());
   }
 }
