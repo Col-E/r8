3dd146258f974c16b3ce4080245c0e427207f973
Rico Wind <ricow@google.com>
2018-08-21 07:47:59 +0200

    Update double cmp test to assert inlining happened
    This ensures that we can actually validate that the method got inlined.
    
    Change-Id: I7197e458af5b7a3c9439be4d1a7e41735e12bb29

--- src/test/java/com/android/tools/r8/cf/InlineCmpDoubleTestRunner.java
+++ src/test/java/com/android/tools/r8/cf/InlineCmpDoubleTestRunner.java
@@ -3,39 +3,70 @@
 // BSD-style license that can be found in the LICENSE file.
 package com.android.tools.r8.cf;
 
+import static org.junit.Assert.assertEquals;
+
 import com.android.tools.r8.ClassFileConsumer.ArchiveConsumer;
 import com.android.tools.r8.CompilationMode;
 import com.android.tools.r8.R8Command;
+import com.android.tools.r8.TestBase;
 import com.android.tools.r8.ToolHelper;
+import com.android.tools.r8.naming.MemberNaming.MethodSignature;
 import com.android.tools.r8.origin.Origin;
+import com.android.tools.r8.utils.AndroidApp;
 import com.android.tools.r8.utils.AndroidAppConsumers;
+import com.android.tools.r8.utils.codeinspector.ClassSubject;
+import com.android.tools.r8.utils.codeinspector.CodeInspector;
+import com.android.tools.r8.utils.codeinspector.MethodSubject;
+import com.google.common.collect.ImmutableList;
 import java.nio.file.Path;
+import java.util.List;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class InlineCmpDoubleTestRunner {
   static final Class CLASS = InlineCmpDoubleTest.class;
+  final boolean enableInlining;
   @Rule public TemporaryFolder temp = ToolHelper.getTemporaryFolderForTest();
+
+  public InlineCmpDoubleTestRunner(boolean enableInlining) {
+    this.enableInlining = enableInlining;
+  }
+
+  @Parameters(name = "inlining={0}")
+  public static Boolean[] data() {
+    return new Boolean[]{true, false};
+  }
 
   @Test
   public void test() throws Exception {
     byte[] inputClass = ToolHelper.getClassAsBytes(CLASS);
     AndroidAppConsumers appBuilder = new AndroidAppConsumers();
     Path outPath = temp.getRoot().toPath().resolve("out.jar");
+    List<String> proguardKeepMain = ImmutableList.of(TestBase.keepMainProguardConfiguration(CLASS));
     R8Command command =  R8Command.builder()
-            .setMode(CompilationMode.RELEASE)
-            .addLibraryFiles(ToolHelper.getJava8RuntimeJar())
-            .setProgramConsumer(appBuilder.wrapClassFileConsumer(new ArchiveConsumer(outPath)))
-            .addClassProgramData(inputClass, Origin.unknown())
-            .build();
+        .setMode(CompilationMode.RELEASE)
+        .addLibraryFiles(ToolHelper.getJava8RuntimeJar())
+        .setProgramConsumer(appBuilder.wrapClassFileConsumer(new ArchiveConsumer(outPath)))
+        .addProguardConfiguration(proguardKeepMain, Origin.unknown())
+        .addClassProgramData(inputClass, Origin.unknown())
+        .build();
 
-    ToolHelper.runR8(command, options -> {
+    AndroidApp app = ToolHelper.runR8(command, options -> {
           options.enableCfFrontend = true;
-          options.enableInlining = true;
-          options.inliningInstructionLimit = 40;
-        });
+          options.enableInlining = enableInlining;
+    });
+
 
     assert ToolHelper.runJava(outPath, CLASS.getCanonicalName()).exitCode == 0;
+    CodeInspector inspector = new CodeInspector(app);
+    ClassSubject clazz = inspector.clazz(CLASS);
+    MethodSubject method = clazz
+        .method(new MethodSignature("inlineMe", "int", ImmutableList.of("int")));
+    assertEquals(enableInlining, !method.isPresent());
   }
 }
