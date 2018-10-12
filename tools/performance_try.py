0091948fa318479f3c8c41b279adfe181a882dc5
Rico Wind <ricow@google.com>
2018-10-12 08:57:10 +0200

    Add convinience script for uploading changes to performance tracking.
    Change-Id: Id49e298eed5128184e854c6e9e828b63697792c3

---
+++ tools/performance_try.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Copyright (c) 2018, the R8 project authors. Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+import os
+import subprocess
+import sys
+import utils
+
+SCRIPT = '/google/data/ro/teams/dart/golem/bin/golem4.dart'
+DART = os.path.join(utils.THIRD_PARTY, 'dart-sdk', 'bin', 'dart')
+
+def Main():
+  args = sys.argv[1:]
+  if len(args) != 1:
+    print('Performance tracking takes exactly one argument, the name for display')
+  subprocess.check_call([DART, SCRIPT, args[0]])
+
+if __name__ == '__main__':
+  sys.exit(Main())
