ab260d77ad575f70d68756dd89017d04f6364c51
Christoffer Quist Adamsen <christofferqa@google.com>
2018-11-19 09:19:13 +0100

    Handle directories that do not exist in -printmapping
    Bug: 119662037
    Change-Id: I486f8ee163b8301915fa464265d8ad2372ddf0b4

---
+++ src/test/java/com/android/tools/r8/proguard/printmapping/PrintMappingTest.java
@@ -0,0 +1,40 @@
+// Copyright (c) 2018, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.proguard.printmapping;
+
+import static org.junit.Assert.assertTrue;
+
+import com.android.tools.r8.TestBase;
+import java.nio.file.Path;
+import org.junit.Test;
+
+public class PrintMappingTest extends TestBase {
+
+  @Test
+  public void testWithExistingDirectory() throws Exception {
+    Path mapping = temp.getRoot().toPath().resolve("existing/directory/mapping.txt");
+    mapping.getParent().toFile().mkdirs();
+    test(mapping);
+  }
+
+  @Test
+  public void testWithNonExistingDirectory() throws Exception {
+    test(temp.getRoot().toPath().resolve("not/a/directory/mapping.txt"));
+  }
+
+  private void test(Path mapping) throws Exception {
+    testForR8(Backend.DEX)
+        .addInnerClasses(PrintMappingTest.class)
+        .addKeepMainRule(TestClass.class)
+        .addKeepRules("-printmapping " + mapping)
+        .compile();
+    assertTrue(mapping.toFile().exists());
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {}
+  }
+}
