9baabd85bbb6d5644dfea2a4ae1ad012412855bb
Christoffer Quist Adamsen <christofferqa@google.com>
2018-12-14 12:31:49 +0100

    Add failing test for class inliner
    Bug: 120182628
    Change-Id: I47cbe495dca7bc3ef03503aa35bb68d17e15354f

---
+++ src/test/java/com/android/tools/r8/ir/optimize/classinliner/BuilderWithInheritanceTest.java
@@ -0,0 +1,92 @@
+// Copyright (c) 2018, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.ir.optimize.classinliner;
+
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.NeverMerge;
+import com.android.tools.r8.TestBase;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/** Regression test for b/120182628. */
+@RunWith(Parameterized.class)
+public class BuilderWithInheritanceTest extends TestBase {
+
+  private final Backend backend;
+
+  @Parameters(name = "Backend: {0}")
+  public static Backend[] data() {
+    return Backend.values();
+  }
+
+  public BuilderWithInheritanceTest(Backend backend) {
+    this.backend = backend;
+  }
+
+  @Ignore("b/120182628")
+  @Test
+  public void test() throws Exception {
+    testForR8(backend)
+        .addInnerClasses(BuilderWithInheritanceTest.class)
+        .addKeepMainRule(TestClass.class)
+        .enableInliningAnnotations()
+        .enableMergeAnnotations()
+        .run(TestClass.class)
+        .assertSuccessWithOutput("42");
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {
+      A a = new Builder(42).build();
+      System.out.print(a.get());
+    }
+  }
+
+  static class A {
+
+    private final int f;
+
+    public A(int f) {
+      this.f = f;
+    }
+
+    public int get() {
+      return f;
+    }
+  }
+
+  @NeverMerge
+  static class BuilderBase {
+
+    protected int f;
+
+    public BuilderBase(int f) {
+      this.f = f;
+    }
+
+    @NeverInline
+    public static int get(BuilderBase obj) {
+      return obj.f;
+    }
+  }
+
+  static class Builder extends BuilderBase {
+
+    public Builder(int f) {
+      super(f);
+    }
+
+    public A build() {
+      // After force inlining of get() there will be a field-read "this.f", which is not allowed by
+      // the class inliner, because the class inliner only handles reads from fields that are
+      // declared on Builder.
+      return new A(get(this));
+    }
+  }
+}
