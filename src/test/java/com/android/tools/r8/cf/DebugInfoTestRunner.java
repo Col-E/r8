90d5b7fb026af44fe26528310c71ceb7e5f6a371
Ian Zerny <zerny@google.com>
2019-01-07 11:50:01 +0100

    Update DebugInfoTestRunner to new testing utils.
    Bug: 77522100
    Change-Id: I57f92e265ce393a469a68976c153cb8dcb40557d

--- src/test/java/com/android/tools/r8/cf/DebugInfoTestRunner.java
+++ src/test/java/com/android/tools/r8/cf/DebugInfoTestRunner.java
@@ -3,62 +3,45 @@
 // BSD-style license that can be found in the LICENSE file.
 package com.android.tools.r8.cf;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import com.android.tools.r8.ClassFileConsumer;
 import com.android.tools.r8.CompilationFailedException;
-import com.android.tools.r8.CompilationMode;
-import com.android.tools.r8.ProgramConsumer;
-import com.android.tools.r8.R8Command;
-import com.android.tools.r8.R8Command.Builder;
+import com.android.tools.r8.R8TestBuilder;
 import com.android.tools.r8.TestBase;
-import com.android.tools.r8.ToolHelper;
-import com.android.tools.r8.ToolHelper.ProcessResult;
-import com.android.tools.r8.origin.Origin;
-import com.android.tools.r8.utils.ThrowingConsumer;
 import java.nio.file.Path;
 import org.junit.Test;
 
 public class DebugInfoTestRunner extends TestBase {
-  static final Class CLASS = DebugInfoTest.class;
+  private static final Class<?> CLASS = DebugInfoTest.class;
+  private static final String EXPECTED = "";
 
   @Test
   public void test() throws Exception {
-    ProcessResult runInput =
-        ToolHelper.runJava(ToolHelper.getClassPathForTests(), CLASS.getCanonicalName());
-    assertEquals(0, runInput.exitCode);
+    testForJvm().addProgramClasses(CLASS).run(CLASS).assertSuccessWithOutput(EXPECTED);
+
     Path out1 = temp.getRoot().toPath().resolve("out1.zip");
-    build(
-        builder -> builder.addClassProgramData(ToolHelper.getClassAsBytes(CLASS), Origin.unknown()),
-        new ClassFileConsumer.ArchiveConsumer(out1));
-    ProcessResult run1 = ToolHelper.runJava(out1, CLASS.getCanonicalName());
-    assertEquals(runInput.toString(), run1.toString());
-    Path out2 = temp.getRoot().toPath().resolve("out2.zip");
-    boolean invalidDebugInfo = false;
+    builder()
+        .addProgramClasses(CLASS)
+        .compile()
+        .writeToZip(out1)
+        .run(CLASS)
+        .assertSuccessWithOutput(EXPECTED);
+
     try {
-      build(builder -> builder.addProgramFiles(out1), new ClassFileConsumer.ArchiveConsumer(out2));
+      builder().addProgramFiles(out1).run(CLASS).assertSuccessWithOutput(EXPECTED);
+      // TODO(b/77522100): Remove once fixed.
+      fail();
     } catch (CompilationFailedException e) {
-      invalidDebugInfo = e.getCause().getMessage().contains("Invalid debug info");
-    }
-    // TODO(b/77522100): Change to assertFalse when fixed.
-    assertTrue(invalidDebugInfo);
-    if (!invalidDebugInfo) {
-      ProcessResult run2 = ToolHelper.runJava(out2, CLASS.getCanonicalName());
-      assertEquals(runInput.toString(), run2.toString());
+      // TODO(b/77522100): Remove one fixed.
+      assert e.getCause().getMessage().contains("Invalid debug info");
     }
   }
 
-  private void build(ThrowingConsumer<Builder, Exception> input, ProgramConsumer consumer)
-      throws Exception {
-    Builder builder =
-        R8Command.builder()
-            .setMode(CompilationMode.DEBUG)
-            .setDisableTreeShaking(true)
-            .setDisableMinification(true)
-            .addLibraryFiles(ToolHelper.getJava8RuntimeJar())
-            .setProgramConsumer(consumer);
-    input.accept(builder);
-    ToolHelper.runR8(builder.build(), o -> o.invalidDebugInfoFatal = true);
+  private R8TestBuilder builder() {
+    return testForR8(Backend.CF)
+        .debug()
+        .noTreeShaking()
+        .noMinification()
+        .addOptionsModification(o -> o.invalidDebugInfoFatal = true);
   }
 }
