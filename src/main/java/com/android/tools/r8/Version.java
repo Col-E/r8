dcad6f09f0dc1524b68763444c27b29cf339e183
Morten Krogh-Jespersen <mkroghj@google.com>
2019-01-21 09:51:27 +0100

    Update patch version for release - new version is 1.5.1-dev
    Change-Id: Id0db51a5bfa6c9ddba68db000b6001a3a6b3e705

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "1.5.0-dev";
+  public static final String LABEL = "1.5.1-dev";
 
   private Version() {
   }
