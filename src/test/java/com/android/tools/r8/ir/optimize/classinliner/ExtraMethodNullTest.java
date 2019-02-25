a14bc20bf2edc8c47c3af00cd5872a0e0163f949
Morten Krogh-Jespersen <mkroghj@google.com>
2019-02-25 10:29:30 +0100

    Add check of null on receiver for extra methods when class-inlining
    Bug: 124842076
    Change-Id: Ie902717b5f16f67a9b521888fe1970746ef793dc

---
+++ src/test/java/com/android/tools/r8/ir/optimize/classinliner/ExtraMethodNullTest.java
@@ -0,0 +1,41 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.ir.optimize.classinliner;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+import com.android.tools.r8.CompilationFailedException;
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestBase;
+import java.io.IOException;
+import org.junit.Test;
+
+public class ExtraMethodNullTest extends TestBase {
+
+  @Test
+  public void test() throws IOException, CompilationFailedException {
+    testForR8(Backend.DEX)
+        .addProgramClassesAndInnerClasses(One.class)
+        .addKeepMainRule(One.class)
+        .run(One.class)
+        .assertFailureWithErrorThatMatches(containsString("java.lang.NullPointerException:"));
+  }
+
+  public static class One {
+
+    public static void main(String[] args) {
+      One one = new One();
+      Other other = args.length == 0 ? null : new Other();
+      other.print(one);
+    }
+
+    static class Other {
+      @NeverInline
+      Object print(Object one) {
+        return one;
+      }
+    }
+  }
+}
