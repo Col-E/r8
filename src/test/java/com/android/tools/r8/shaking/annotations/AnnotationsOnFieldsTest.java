840256ec580da20a44a662674a89b2a919a1475a
Christoffer Quist Adamsen <christofferqa@google.com>
2019-03-12 11:07:43 +0100

    Fix typo in AnnotationsOnFieldsTest
    Bug: 128387933
    Change-Id: Icb6d8a7ec19ff9734bf1c4355c2e1cfc60b43996

--- src/test/java/com/android/tools/r8/shaking/annotations/AnnotationsOnFieldsTest.java
+++ src/test/java/com/android/tools/r8/shaking/annotations/AnnotationsOnFieldsTest.java
@@ -1,23 +1,22 @@
 package com.android.tools.r8.shaking.annotations;
 
-import com.android.tools.r8.NeverClassInline;
-import com.android.tools.r8.TestBase;
-import com.android.tools.r8.utils.codeinspector.ClassSubject;
-import com.android.tools.r8.utils.codeinspector.FieldSubject;
-import com.google.common.collect.ImmutableList;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import java.util.List;
-
 import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
 import static com.android.tools.r8.utils.codeinspector.Matchers.isRenamed;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static org.junit.Assert.assertThat;
+
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.utils.codeinspector.ClassSubject;
+import com.android.tools.r8.utils.codeinspector.FieldSubject;
+import com.google.common.collect.ImmutableList;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
 public class AnnotationsOnFieldsTest extends TestBase {
@@ -52,21 +51,23 @@ public class AnnotationsOnFieldsTest extends TestBase {
         .addKeepRules("-keepclassmembers class * { @**.*Annotation <fields>; }")
         .addKeepRules("-keepattributes *Annotation*")
         .compile()
-        .inspect(inspector -> {
-          ClassSubject clazz = inspector.clazz(TestClass.class);
-          assertThat(clazz, isRenamed());
+        .inspect(
+            inspector -> {
+              ClassSubject clazz = inspector.clazz(TestClass.class);
+              assertThat(clazz, isRenamed());
 
-          FieldSubject field = clazz.uniqueFieldWithName("field");
-          assertThat(field, isPresent());
-          assertThat(field.annotation(FieldAnnotation.class.getTypeName()), isPresent());
-          assertThat(inspector.clazz(FieldAnnotationUse.class), isRenamed());
+              FieldSubject field = clazz.uniqueFieldWithName("field");
+              assertThat(field, isPresent());
+              assertThat(field.annotation(FieldAnnotation.class.getTypeName()), isPresent());
+              assertThat(inspector.clazz(FieldAnnotationUse.class), isRenamed());
 
-          FieldSubject staticField = clazz.uniqueFieldWithName("staticField");
-          assertThat(staticField, isPresent());
-          assertThat(staticField.annotation(StaticFieldAnnotation.class.getTypeName()), isPresent());
-          assertThat(inspector.clazz(StaticFieldAnnotationUse.class), isRenamed());
-        })
-        .run(TestClass.class)
+              FieldSubject staticField = clazz.uniqueFieldWithName("staticField");
+              assertThat(staticField, isPresent());
+              assertThat(
+                  staticField.annotation(StaticFieldAnnotation.class.getTypeName()), isPresent());
+              assertThat(inspector.clazz(StaticFieldAnnotationUse.class), isRenamed());
+            })
+        .run(MainClass.class)
         .assertSuccess();
   }
 
