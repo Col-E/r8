d1fa3a72d03afbe4aa2a3ab90556b60faf81b5dc
Christoffer Quist Adamsen <christofferqa@google.com>
2019-03-14 17:42:09 +0100

    Add a test for inliner bug leading to IllegalAccessError
    Bug: 128604123
    Change-Id: Ic6dbca03308d0cac16ad6b91974b9dcddd65b990

---
+++ src/test/java/com/android/tools/r8/ir/optimize/inliner/InlineNonReboundFieldTest.java
@@ -0,0 +1,49 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.ir.optimize.inliner;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.ir.optimize.inliner.testclasses.Greeting;
+import org.junit.Test;
+
+/** Regression test for b/128604123. */
+public class InlineNonReboundFieldTest extends TestBase {
+
+  @Test
+  public void test() throws Exception {
+    testForR8(Backend.DEX)
+        .addProgramClasses(
+            TestClass.class, Greeter.class, Greeting.class, Greeting.getGreetingBase())
+        .addKeepMainRule(TestClass.class)
+        .enableClassInliningAnnotations()
+        .enableMergeAnnotations()
+        .run(TestClass.class)
+        .assertFailureWithErrorThatMatches(containsString("IllegalAccessError"));
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {
+      new Greeter("Hello world!").greet();
+    }
+  }
+
+  @NeverClassInline
+  static class Greeter extends Greeting {
+
+    static String TAG = "Greeter";
+
+    Greeter(String greeting) {
+      this.greeting = greeting;
+    }
+
+    void greet() {
+      System.out.println(TAG + ": " + greeting);
+    }
+  }
+}
