69d12c7fe72627dfe99f0b1883d91d92c3c3c61e
Jinseong Jeon <jsjeon@google.com>
2019-03-19 00:28:42 -0700

    Add tests for string concatenation optimization.
    Bug: 114002137
    Change-Id: I718f15e525b3296c60fcebc0abb2c97856228d89

---
+++ src/test/java/com/android/tools/r8/ir/optimize/string/StringConcatenationTest.java
@@ -0,0 +1,147 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.ir.optimize.string;
+
+import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeTrue;
+
+import com.android.tools.r8.D8TestRunResult;
+import com.android.tools.r8.R8TestRunResult;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestRunResult;
+import com.android.tools.r8.utils.StringUtils;
+import com.android.tools.r8.utils.codeinspector.ClassSubject;
+import com.android.tools.r8.utils.codeinspector.CodeInspector;
+import com.android.tools.r8.utils.codeinspector.InstructionSubject.JumboStringMode;
+import com.android.tools.r8.utils.codeinspector.MethodSubject;
+import com.google.common.collect.Streams;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class StringConcatenationTest extends TestBase {
+  private static final Class<?> MAIN = StringConcatenationTestClass.class;
+  private static final String JAVA_OUTPUT = StringUtils.lines(
+      "xyz",
+      "42",
+      "0.14 0 false null",
+      "Hello,R8",
+      "Hello,R8",
+      "Hello,",
+      "Hello,D8",
+      "na;na;na;na;na;na;na;na;Batman!",
+      "na;na;na;na;na;na;na;na;Batman!"
+  );
+
+  @Parameterized.Parameters(name = "Backend: {0}")
+  public static Backend[] data() {
+    return Backend.values();
+  }
+
+  private final Backend backend;
+
+  public StringConcatenationTest(Backend backend) {
+    this.backend = backend;
+  }
+
+  @Test
+  public void testJVMoutput() throws Exception {
+    assumeTrue("Only run JVM reference once (for CF backend)", backend == Backend.CF);
+    testForJvm().addTestClasspath().run(MAIN).assertSuccessWithOutput(JAVA_OUTPUT);
+  }
+
+  private void test(
+      TestRunResult result, int expectedStringCount1, int expectedStringCount2)
+      throws Exception {
+    CodeInspector codeInspector = result.inspector();
+    ClassSubject mainClass = codeInspector.clazz(MAIN);
+
+    MethodSubject method = mainClass.uniqueMethodWithName("trivialSequence");
+    assertThat(method, isPresent());
+    long count = Streams.stream(method.iterateInstructions(
+        i -> i.isConstString(JumboStringMode.ALLOW))).count();
+    assertEquals(expectedStringCount1, count);
+
+    method = mainClass.uniqueMethodWithName("nonStringArgs");
+    assertThat(method, isPresent());
+    count = Streams.stream(method.iterateInstructions(
+        i -> i.isConstString(JumboStringMode.ALLOW))).count();
+    assertEquals(0, count);
+
+    method = mainClass.uniqueMethodWithName("typeConversion");
+    assertThat(method, isPresent());
+    count = Streams.stream(method.iterateInstructions(
+        i -> i.isConstString(JumboStringMode.ALLOW))).count();
+    assertEquals(0, count);
+
+    method = mainClass.uniqueMethodWithName("nestedBuilders_appendBuilderItself");
+    assertThat(method, isPresent());
+    count = Streams.stream(method.iterateInstructions(
+        i -> i.isConstString(JumboStringMode.ALLOW))).count();
+    assertEquals(expectedStringCount2, count);
+
+    method = mainClass.uniqueMethodWithName("nestedBuilders_appendBuilderResult");
+    assertThat(method, isPresent());
+    count = Streams.stream(method.iterateInstructions(
+        i -> i.isConstString(JumboStringMode.ALLOW))).count();
+    assertEquals(expectedStringCount2, count);
+
+    method = mainClass.uniqueMethodWithName("simplePhi");
+    assertThat(method, isPresent());
+    count = Streams.stream(method.iterateInstructions(
+        i -> i.isConstString(JumboStringMode.ALLOW))).count();
+    assertEquals(5, count);
+
+    method = mainClass.uniqueMethodWithName("loop");
+    assertThat(method, isPresent());
+    count = Streams.stream(method.iterateInstructions(
+        i -> i.isConstString(JumboStringMode.ALLOW))).count();
+    assertEquals(3, count);
+
+    method = mainClass.uniqueMethodWithName("loopWithBuilder");
+    assertThat(method, isPresent());
+    count = Streams.stream(method.iterateInstructions(
+        i -> i.isConstString(JumboStringMode.ALLOW))).count();
+    assertEquals(2, count);
+  }
+
+  @Test
+  public void testD8() throws Exception {
+    assumeTrue("Only run D8 for Dex backend", backend == Backend.DEX);
+
+    D8TestRunResult result = testForD8()
+        .debug()
+        .addProgramClasses(MAIN)
+        .run(MAIN)
+        .assertSuccessWithOutput(JAVA_OUTPUT);
+    test(result, 3, 4);
+
+    result = testForD8()
+        .release()
+        .addProgramClasses(MAIN)
+        .run(MAIN)
+        .assertSuccessWithOutput(JAVA_OUTPUT);
+    // TODO(b/114002137): could be 1 and 3.
+    test(result, 3, 4);
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    // CF should not canonicalize strings or lower them. See (r8g/30163) and (r8g/30320).
+    assumeTrue(backend == Backend.DEX);
+    R8TestRunResult result = testForR8(backend)
+        .addProgramClasses(MAIN)
+        .enableInliningAnnotations()
+        .addKeepMainRule(MAIN)
+        .noMinification()
+        .run(MAIN)
+        .assertSuccessWithOutput(JAVA_OUTPUT);
+    // TODO(b/114002137): could be 1 and 3.
+    test(result, 3, 4);
+  }
+
+}
---
+++ src/test/java/com/android/tools/r8/ir/optimize/string/StringConcatenationTestClass.java
@@ -0,0 +1,115 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.ir.optimize.string;
+
+import com.android.tools.r8.NeverInline;
+
+class StringConcatenationTestClass {
+  @NeverInline
+  public static void trivialSequence() {
+    String x = "x";
+    String y = "y";
+    String z = "z";
+    System.out.println(x + y + z);
+  }
+
+  @NeverInline
+  public static void nonStringArgs() {
+    StringBuilder builder = new StringBuilder();
+    builder.append(4);
+    builder.append(2);
+    System.out.println(builder.toString());
+  }
+
+  @NeverInline
+  public static void typeConversion() {
+    StringBuilder builder = new StringBuilder();
+    float f = 0.14f;
+    builder.append(f);
+    builder.append(' ');
+    int i = (int) f;
+    builder.append(i);
+    builder.append(' ');
+
+    boolean b = false;
+    builder.append(b);
+    builder.append(' ');
+    Object n = null;
+    builder.append(n);
+    System.out.println(builder.toString());
+  }
+
+  @NeverInline
+  public static void nestedBuilders_appendBuilderItself() {
+    StringBuilder b1 = new StringBuilder();
+    b1.append("Hello");
+    b1.append(",");
+    StringBuilder b2 = new StringBuilder();
+    b2.append("R");
+    // TODO(b/114002137): switch to use the integer.
+    b2.append("8");
+    b1.append(b2);
+    System.out.println(b1.toString());
+  }
+
+  @NeverInline
+  public static void nestedBuilders_appendBuilderResult() {
+    StringBuilder b1 = new StringBuilder();
+    b1.append("Hello");
+    b1.append(",");
+    StringBuilder b2 = new StringBuilder();
+    b2.append("R");
+    // TODO(b/114002137): switch to use the integer.
+    b2.append("8");
+    b1.append(b2.toString());
+    System.out.println(b1.toString());
+  }
+
+  @NeverInline
+  public static void simplePhi() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("Hello");
+    builder.append(",");
+    System.out.println(builder.toString());
+
+    if (System.currentTimeMillis() > 0) {
+      builder.append("D");
+    } else {
+      builder.append("R");
+    }
+    // TODO(b/114002137): switch to use the integer.
+    builder.append("8");
+    System.out.println(builder.toString());
+  }
+
+  @NeverInline
+  public static void loop() {
+    String r = "";
+    for (int i = 0; i < 8; i++) {
+      r = r + "na;";
+    }
+    System.out.println(r + "Batman!");
+  }
+
+  @NeverInline
+  public static void loopWithBuilder() {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < 8; i++) {
+      builder.append("na;");
+    }
+    builder.append("Batman!");
+    System.out.println(builder.toString());
+  }
+
+  public static void main(String[] args) {
+    trivialSequence();
+    nonStringArgs();
+    typeConversion();
+    nestedBuilders_appendBuilderItself();
+    nestedBuilders_appendBuilderResult();
+    simplePhi();
+    loop();
+    loopWithBuilder();
+  }
+}
