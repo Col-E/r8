436e2f6fb3f58daefe975d71560b8f3cb657902d
Morten Krogh-Jespersen <mkroghj@google.com>
2019-03-27 15:15:40 +0100

    Add test for inlining of kept method by the ClassInliner
    The ClassInliner will inline a method even if it specified as kept.
    We will not be able to get rid of the method anyway, and it may fail if
    the class implementation is changed on runtime.
    
    Bug: 129390240
    Change-Id: I190d37054501e3dc41e9c0d31873309891008864

---
+++ src/test/java/com/android/tools/r8/ir/optimize/classinliner/ClassInlineKeepMethodTest.java
@@ -0,0 +1,78 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.ir.optimize.classinliner;
+
+import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.android.tools.r8.CompilationFailedException;
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.NeverMerge;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.ToolHelper.DexVm.Version;
+import com.android.tools.r8.utils.codeinspector.ClassSubject;
+import com.android.tools.r8.utils.codeinspector.CodeInspector;
+import com.android.tools.r8.utils.codeinspector.InstructionSubject.JumboStringMode;
+import com.android.tools.r8.utils.codeinspector.MethodSubject;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ClassInlineKeepMethodTest extends TestBase {
+
+  private final TestParameters parameters;
+  private static final String EXPECTED_OUTPUT = "Hello world";
+
+  @NeverMerge
+  public static class ShouldBeKept {
+
+    @NeverInline
+    public void shouldBeKept() {
+      System.out.print("Hello world");
+    }
+  }
+
+  public static class Keeper {
+
+    public static void main(String[] args) {
+      new ShouldBeKept().shouldBeKept();
+    }
+  }
+
+  @Parameterized.Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withDexRuntime(Version.last()).build();
+  }
+
+  public ClassInlineKeepMethodTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Ignore("b/129390240")
+  @Test
+  public void testIsKeptWithName()
+      throws ExecutionException, CompilationFailedException, IOException {
+    CodeInspector inspector = testForR8(parameters.getBackend())
+        .addInnerClasses(ClassInlineKeepMethodTest.class)
+        .addKeepMainRule(Keeper.class)
+        .addKeepClassAndMembersRules(ShouldBeKept.class)
+        .enableInliningAnnotations()
+        .enableMergeAnnotations()
+        .run(parameters.getRuntime(), Keeper.class)
+        .assertSuccessWithOutput(EXPECTED_OUTPUT)
+        .inspector();
+    ClassSubject clazz = inspector.clazz(Keeper.class);
+    assertThat(clazz, isPresent());
+    MethodSubject main = clazz.uniqueMethodWithName("main");
+    assertTrue(main.streamInstructions().noneMatch(i -> i.isConstString(JumboStringMode.ALLOW)));
+  }
+}
