59d782de65059e5df43772ce53658a876dce6f5a
Christoffer Quist Adamsen <christofferqa@google.com>
2019-03-27 07:12:23 +0000

    Revert "Revert "Minor changes to -useuniqueclassmembernames tests""
    This reverts commit 4cc88e0b7cd050c57be6accf37b77bcf669cfd9c.
    
    Reason for revert: The CL does not introduce test failures.
    
    Change-Id: I0a8cdc799e15e1a2c4ba1014b16ebd6ebbbe70d1

--- src/test/java/com/android/tools/r8/naming/uniqueclassmembernames/UseUniqueMemberNameTest.java
+++ src/test/java/com/android/tools/r8/naming/uniqueclassmembernames/UseUniqueMemberNameTest.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2017, the R8 project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-package com.android.tools.r8.naming;
+package com.android.tools.r8.naming.uniqueclassmembernames;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -10,6 +10,8 @@ import com.android.tools.r8.graph.DexField;
 import com.android.tools.r8.graph.DexItemFactory;
 import com.android.tools.r8.graph.DexMethod;
 import com.android.tools.r8.graph.DexType;
+import com.android.tools.r8.naming.NamingLens;
+import com.android.tools.r8.naming.NamingTestBase;
 import com.android.tools.r8.utils.ListUtils;
 import com.android.tools.r8.utils.Timing;
 import java.nio.file.Paths;
