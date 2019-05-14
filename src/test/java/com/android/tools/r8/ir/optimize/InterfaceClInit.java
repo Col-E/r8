95a5984733a2773bd08ec55d489861461f1e4303
Rico Wind <ricow@google.com>
2019-05-14 14:20:38 +0200

    Add test for wrong clinit order when using a static field in an interface
    Bug: 132676467
    Change-Id: I127c8f9daecbf29e24b84430d434f603bb71d896

---
+++ src/test/java/com/android/tools/r8/ir/optimize/InterfaceClInit.java
@@ -0,0 +1,70 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.ir.optimize;
+
+import static org.junit.Assume.assumeTrue;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.TestRunResult;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class InterfaceClInit extends TestBase {
+  static String EXPECTED= "i = 42";
+  private final TestParameters parameters;
+
+
+  @Parameterized.Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimes().build();
+  }
+
+
+  public InterfaceClInit(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Ignore("b/132676467")
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addProgramClasses(TestClass.class, InterfaceWithStaticBlock.class, UsedFromStatic.class)
+        .addKeepMainRule(TestClass.class)
+        .run(parameters.getRuntime(), TestClass.class)
+        .assertSuccessWithOutputLines(EXPECTED);
+  }
+
+  @Test
+  public void testJvm() throws Exception {
+    assumeTrue(parameters.isCfRuntime());
+    testForJvm()
+        .addProgramClasses(TestClass.class, InterfaceWithStaticBlock.class, UsedFromStatic.class)
+        .run(parameters.getRuntime(), TestClass.class)
+        .assertSuccessWithOutputLines(EXPECTED);
+  }
+}
+
+class TestClass implements InterfaceWithStaticBlock {
+  @Override
+  public void foobar() { }
+
+  public static void main(String[] args) {
+    UsedFromStatic.i = 42;
+    System.out.println("i = " + InterfaceWithStaticBlock.i);
+  }
+}
+
+interface InterfaceWithStaticBlock {
+  int i = UsedFromStatic.i;
+  void foobar();
+}
+
+class UsedFromStatic {
+  public static int i = 60;
+}
