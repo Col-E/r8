0d6ab49abae64cbc7cbe157a602bf41dd7681dda
Christoffer Quist Adamsen <christofferqa@google.com>
2019-06-05 11:27:42 +0200

    Disable value propagation for constructors
    Removing a call to a constructor that is marked as having no side effects can lead to verification errors, due to uninitialized instances being used.
    
    This was caught on YouTube.
    
    Change-Id: I84ee50131a4d6f61514c090485ddddd6f2e3f841

--- src/main/java/com/android/tools/r8/ir/optimize/MemberValuePropagation.java
+++ src/main/java/com/android/tools/r8/ir/optimize/MemberValuePropagation.java
@@ -254,6 +254,13 @@ public class MemberValuePropagation {
       return;
     }
     DexEncodedMethod definition = current.lookupSingleTarget(appView, callingContext);
+    if (definition != null && definition.isInstanceInitializer()) {
+      // Member value propagation does not apply to constructors. Removing a call to a constructor
+      // that is marked as having no side effects could lead to verification errors, due to
+      // uninitialized instances being used.
+      return;
+    }
+
     ProguardMemberRuleLookup lookup = lookupMemberRule(definition);
     if (lookup == null) {
       // Since -assumenosideeffects rules are always applied to all overriding methods, we can
@@ -268,8 +275,10 @@ public class MemberValuePropagation {
       if (lookup.type == RuleType.ASSUME_NO_SIDE_EFFECTS && outValueNullOrNotUsed) {
         // Remove invoke if marked as having no side effects and the return value is not used.
         iterator.removeOrReplaceByDebugLocalRead();
-        invokeReplaced = true;
-      } else if (!outValueNullOrNotUsed) {
+        return;
+      }
+
+      if (!outValueNullOrNotUsed) {
         // Check to see if a constant value can be assumed.
         invokeReplaced =
             tryConstantReplacementFromProguard(
---
+++ src/test/java/com/android/tools/r8/ir/optimize/membervaluepropagation/NoConstructorPropagationTest.java
@@ -0,0 +1,62 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.ir.optimize.membervaluepropagation;
+
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class NoConstructorPropagationTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimes().build();
+  }
+
+  public NoConstructorPropagationTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(NoConstructorPropagationTest.class)
+        .addKeepMainRule(TestClass.class)
+        .addKeepRules(
+            "-assumenosideeffects class " + Greeter.class.getTypeName() + " {",
+            "  <init>(...);",
+            "}")
+        .enableClassInliningAnnotations()
+        .enableInliningAnnotations()
+        .setMinApi(parameters.getRuntime())
+        .run(parameters.getRuntime(), TestClass.class)
+        .assertSuccessWithOutputLines("Hello world!");
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {
+      new Greeter().greet();
+    }
+  }
+
+  @NeverClassInline
+  static class Greeter {
+
+    @NeverInline
+    public void greet() {
+      System.out.println("Hello world!");
+    }
+  }
+}
