09404deb24ee5ba39a6a1f81cdfca3af0d57eafe
Christoffer Quist Adamsen <christofferqa@google.com>
2019-06-19 18:40:21 +0200

    Add -assumevalues test for constant valued field
    Change-Id: Iba736b75757e56a42c39e5a1023846447d98a34e

---
+++ src/test/java/com/android/tools/r8/shaking/assumevalues/AssumeValuesForConstantValuedFieldTest.java
@@ -0,0 +1,78 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.shaking.assumevalues;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class AssumeValuesForConstantValuedFieldTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimes().build();
+  }
+
+  public AssumeValuesForConstantValuedFieldTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(AssumeValuesForConstantValuedFieldTest.class)
+        .addKeepMainRule(TestClass.class)
+        .addKeepRules("-assumevalues class * { static boolean field return false; }")
+        .setMinApi(parameters.getRuntime())
+        .compile()
+        .run(parameters.getRuntime(), TestClass.class)
+        .assertSuccessWithOutputLines(
+            "TestClass.field = false", "A.field = false", "B.<clinit>()", "B.field = false");
+  }
+
+  static class TestClass {
+
+    static boolean field = true;
+
+    public static void main(String[] args) {
+      if (field) {
+        System.out.println("TestClass.field = true");
+      } else {
+        System.out.println("TestClass.field = false");
+      }
+      if (A.field) {
+        System.out.println("A.field = true");
+      } else {
+        System.out.println("A.field = false");
+      }
+      if (B.field) {
+        System.out.println("B.field = true");
+      } else {
+        System.out.println("B.field = false");
+      }
+    }
+  }
+
+  static class A {
+
+    static boolean field = true;
+  }
+
+  static class B {
+
+    static {
+      System.out.println("B.<clinit>()");
+    }
+
+    static boolean field = true;
+  }
+}
