3acf434ee180ac2c75d12475560089f1c4ce0bc8
Morten Krogh-Jespersen <mkroghj@google.com>
2019-07-10 13:22:37 +0200

    Disable inlining of methods with nullable receivers
    Disable until performance regression has been resolved.
    
    Bug: 137174854
    Change-Id: Iec3da6d6755122453f5d1d4358ed68290461869a

--- src/main/java/com/android/tools/r8/utils/InternalOptions.java
+++ src/main/java/com/android/tools/r8/utils/InternalOptions.java
@@ -173,9 +173,10 @@ public class InternalOptions {
   public boolean enableInliningOfInvokesWithDefinitelyNullReceivers =
       System.getProperty("com.android.tools.r8.disableInliningOfInvokesWithDefinitelyNullReceivers")
           == null;
+  // TODO(137174854) Disable until performance regression is investigated.
   public boolean enableInliningOfInvokesWithNullableReceivers =
-      System.getProperty("com.android.tools.r8.disableInliningOfInvokesWithNullableReceivers")
-          == null;
+      System.getProperty("com.android.tools.r8.enableInliningOfInvokesWithNullableReceivers")
+          != null;
   public boolean enableClassInlining = true;
   public boolean enableClassStaticizer = true;
   public boolean enableInitializedClassesAnalysis = true;
