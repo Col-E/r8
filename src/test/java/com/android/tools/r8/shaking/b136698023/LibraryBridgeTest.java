7ffb937447dc9f02ea1aaad5e2106981571a3ad0
Morten Krogh-Jespersen <mkroghj@google.com>
2019-07-12 19:39:14 +0200

    Add test for removal of bridge for implicitly inherited library method
    Bug: 136698023
    Change-Id: Ibe09f34e40bb5dcd811f1cd0bcaac4d3d5ec8bb2

---
+++ src/test/java/com/android/tools/r8/shaking/b136698023/LibraryBridgeTest.java
@@ -0,0 +1,163 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.shaking.b136698023;
+
+import static org.hamcrest.core.StringContains.containsString;
+
+import com.android.tools.r8.CompilationFailedException;
+import com.android.tools.r8.R8TestCompileResult;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class LibraryBridgeTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  // Reproduction of b/136698023 using a the standard library.
+  public interface ICloneable {
+
+    Object clone();
+  }
+
+  public abstract static class BaseOrdinaryClone implements ICloneable {
+
+    @Override
+    public abstract Object clone();
+  }
+
+  public static class MainWithOrdinaryClone {
+
+    public static void main(String[] args) {
+      System.out.println(BaseOrdinaryClone.class.toString());
+    }
+  }
+
+  // Reproduction of b/136698023 using a custom library object.
+  public static class Base {
+
+    protected Object xClone() {
+      return null;
+    }
+  }
+
+  public interface XCloneable {
+
+    Object xClone();
+  }
+
+  public abstract static class Model extends Base implements XCloneable {
+
+    @Override
+    public abstract Object xClone();
+  }
+
+  public static class Main {
+
+    public static void main(String[] args) {
+      System.out.println(Model.class.toString());
+    }
+  }
+
+  // Reproduction of b/136698023 with an implementation as well.
+
+  public static class ModelImpl extends Base implements XCloneable {
+
+    @Override
+    public Object xClone() {
+      return new ModelImpl();
+    }
+  }
+
+  public static class MainImpl {
+
+    public static void main(String[] args) {
+      System.out.println(ModelImpl.class.toString());
+    }
+  }
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimes().build();
+  }
+
+  public LibraryBridgeTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  @Ignore("b/136698023")
+  public void testRemovingClone()
+      throws ExecutionException, CompilationFailedException, IOException {
+    testForR8(parameters.getBackend())
+        .addProgramClasses(ICloneable.class, BaseOrdinaryClone.class, MainWithOrdinaryClone.class)
+        .addKeepClassAndMembersRules(ICloneable.class)
+        .addKeepMainRule(MainWithOrdinaryClone.class)
+        .noMinification()
+        .setMinApi(parameters.getRuntime())
+        .run(parameters.getRuntime(), MainWithOrdinaryClone.class)
+        .assertSuccessWithOutputThatMatches(containsString(BaseOrdinaryClone.class.getTypeName()));
+  }
+
+  @Test
+  @Ignore("b/136698023")
+  public void testRemovingXClone()
+      throws ExecutionException, CompilationFailedException, IOException {
+    testForR8(parameters.getBackend())
+        .addProgramClasses(XCloneable.class, Model.class, Main.class, Base.class)
+        .addKeepClassAndMembersRules(XCloneable.class, Base.class)
+        .addKeepMainRule(Main.class)
+        .noMinification()
+        .setMinApi(parameters.getRuntime())
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputThatMatches(containsString(Model.class.getTypeName()));
+  }
+
+  @Test
+  @Ignore("b/136698023")
+  public void testRemovingXCloneImpl()
+      throws ExecutionException, CompilationFailedException, IOException {
+    testForR8(parameters.getBackend())
+        .addProgramClasses(XCloneable.class, ModelImpl.class, MainImpl.class, Base.class)
+        .addKeepClassAndMembersRules(XCloneable.class, Base.class)
+        .addKeepMainRule(MainImpl.class)
+        .noMinification()
+        .setMinApi(parameters.getRuntime())
+        .run(parameters.getRuntime(), MainImpl.class)
+        .assertSuccessWithOutputThatMatches(containsString(Model.class.getTypeName()));
+  }
+
+  @Test
+  @Ignore("b/136698023")
+  public void testRemovingXCloneWithDefinitionInLibrary()
+      throws ExecutionException, CompilationFailedException, IOException {
+    R8TestCompileResult library =
+        testForR8(parameters.getBackend())
+            .addProgramClasses(Base.class)
+            .addKeepAllClassesRule()
+            .setMinApi(parameters.getRuntime())
+            .compile();
+    testForR8(parameters.getBackend())
+        .addProgramClasses(XCloneable.class, Model.class, Main.class)
+        .addLibraryClasses(Base.class)
+        .addLibraryFiles(runtimeJar(parameters.getBackend()))
+        .addKeepClassAndMembersRules(XCloneable.class)
+        .addKeepMainRule(Main.class)
+        .setMinApi(parameters.getRuntime())
+        .noMinification()
+        .compile()
+        .addRunClasspathFiles(library.writeToZip())
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputThatMatches(containsString(Model.class.getTypeName()));
+  }
+}
---
+++ src/test/java/com/android/tools/r8/shaking/b136698023/ProgramInterfaceNotImplementedTest.java
@@ -0,0 +1,164 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.shaking.b136698023;
+
+import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertTrue;
+
+import com.android.tools.r8.CompilationFailedException;
+import com.android.tools.r8.R8TestCompileResult;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.codeinspector.ClassSubject;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ProgramInterfaceNotImplementedTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  public interface I {
+
+    Object foo();
+  }
+
+  public abstract static class A implements I {
+
+    @Override
+    public abstract Object foo();
+  }
+
+  public abstract static class B implements I {
+
+    @Override
+    public Object foo() {
+      return "FOO";
+    }
+  }
+
+  public static class C implements I {
+
+    @Override
+    public Object foo() {
+      return "FOO";
+    }
+  }
+
+  public abstract static class Lib {
+    public abstract Object foo();
+  }
+
+  public static class D extends Lib implements I {
+
+    @Override
+    public Object foo() {
+      return "FOO";
+    }
+  }
+
+  public static class MainA {
+
+    public static void main(String[] args) {
+      System.out.println(A.class.toString());
+    }
+  }
+
+  public static class MainB {
+
+    public static void main(String[] args) {
+      System.out.println(B.class.toString());
+    }
+  }
+
+  public static class MainC {
+
+    public static void main(String[] args) {
+      System.out.println(C.class.toString());
+    }
+  }
+
+  public static class MainD {
+
+    public static void main(String[] args) {
+      System.out.println(D.class.toString());
+    }
+  }
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimes().build();
+  }
+
+  public ProgramInterfaceNotImplementedTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void testRemovingMethodInA()
+      throws ExecutionException, CompilationFailedException, IOException {
+    test(A.class, MainA.class);
+  }
+
+  @Test
+  public void testRemovingMethodInB()
+      throws ExecutionException, CompilationFailedException, IOException {
+    test(B.class, MainB.class);
+  }
+
+  @Test
+  public void testRemovingMethodInC()
+      throws ExecutionException, CompilationFailedException, IOException {
+    test(C.class, MainC.class);
+  }
+
+  private void test(Class<?> clazz, Class<?> main)
+      throws ExecutionException, CompilationFailedException, IOException {
+    testForR8(parameters.getBackend())
+        .addProgramClasses(I.class, clazz, main)
+        .addKeepClassAndMembersRules(I.class)
+        .addKeepMainRule(main)
+        .noMinification()
+        .setMinApi(parameters.getRuntime())
+        .run(parameters.getRuntime(), main)
+        .assertSuccessWithOutputThatMatches(containsString(clazz.getTypeName()))
+        .inspect(
+            codeInspector -> {
+              ClassSubject foundClazz = codeInspector.clazz(clazz);
+              assertThat(foundClazz, isPresent());
+              assertTrue(foundClazz.allMethods().isEmpty());
+            });
+  }
+
+  @Test
+  public void testRemovingMethodInD()
+      throws ExecutionException, CompilationFailedException, IOException {
+    R8TestCompileResult library =
+        testForR8(parameters.getBackend())
+            .addProgramClasses(Lib.class)
+            .addKeepAllClassesRule()
+            .setMinApi(parameters.getRuntime())
+            .compile();
+    testForR8(parameters.getBackend())
+        .addProgramClasses(I.class, D.class, MainD.class)
+        .addLibraryClasses(Lib.class)
+        .addLibraryFiles(runtimeJar(parameters.getBackend()))
+        .addKeepClassAndMembersRules(I.class)
+        .addKeepMainRule(MainD.class)
+        .setMinApi(parameters.getRuntime())
+        .noMinification()
+        .compile()
+        .addRunClasspathFiles(library.writeToZip())
+        .run(parameters.getRuntime(), MainD.class)
+        .assertSuccessWithOutputThatMatches(containsString(D.class.getTypeName()));
+  }
+}
