28bcef50ac8597cf22e892b1bc3a3b92b74e70e9
Morten Krogh-Jespersen <mkroghj@google.com>
2019-09-23 08:36:06 +0200

    Update patch version for release - new version is 1.6.32-dev
    Change-Id: I3425f25b6efe300f8427928347274845f8bc3be4

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "1.6.31-dev";
+  public static final String LABEL = "1.6.32-dev";
 
   private Version() {
   }
