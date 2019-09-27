69d738ede6c0bd60c874b600c9734f84ba54f794
Ian Zerny <zerny@google.com>
2019-09-27 12:03:19 +0200

    Test for class inlining of kept classes.
    Bug: 141717378
    Change-Id: I39f70fb85a64d9130f48091109cd241411e3b3ca

---
+++ src/test/java/com/android/tools/r8/ir/optimize/classinliner/KeptClassInliningTest.java
@@ -0,0 +1,68 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.ir.optimize.classinliner;
+
+import static com.android.tools.r8.references.Reference.methodFromMethod;
+import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.codeinspector.CodeInspector;
+import com.android.tools.r8.utils.codeinspector.MethodSubject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class KeptClassInliningTest extends TestBase {
+
+  public static class KeptClass {
+
+    public void used() {
+      System.out.println("used()");
+    }
+  }
+
+  public static class Main {
+
+    public static void main(String[] args) {
+      new KeptClass().used();
+    }
+  }
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  public final TestParameters parameters;
+
+  public KeptClassInliningTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void test() throws Exception {
+    CodeInspector inspector =
+        testForR8(parameters.getBackend())
+            .addProgramClasses(KeptClass.class, Main.class)
+            .addKeepMainRule(Main.class)
+            .addKeepClassRules(KeptClass.class)
+            .setMinApi(parameters.getApiLevel())
+            .run(parameters.getRuntime(), Main.class)
+            .assertSuccessWithOutputLines("used()")
+            .inspector();
+    assertThat(inspector.clazz(KeptClass.class), isPresent());
+    MethodSubject main =
+        inspector.method(methodFromMethod(Main.class.getMethod("main", String[].class)));
+    assertFalse(
+        "b/141717378: The class inliner should not inline a kept class.",
+        main.streamInstructions()
+            .anyMatch(i -> i.isInvoke() && i.getMethod().name.toString().equals("used")));
+  }
+}
