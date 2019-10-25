903098827f4f631cd2d599891fca4db3d572c73c
Clément Béra <clementbera@google.com>
2019-10-25 08:40:02 +0000

    Revert "Fix red test"
    This reverts commit 3cb03d12c91f348ab060c3d3ac466d70d0e4a114.
    
    Reason for revert: Submitted without sync
    
    Change-Id: Ic68db86403926a755f51030c84754f10a1b87ab5

--- src/test/java/com/android/tools/r8/desugar/corelib/HelloWorldCompiledOnArtTest.java
+++ src/test/java/com/android/tools/r8/desugar/corelib/HelloWorldCompiledOnArtTest.java
@@ -6,7 +6,6 @@ package com.android.tools.r8.desugar.corelib;
 
 import static com.android.tools.r8.utils.FileUtils.JAR_EXTENSION;
 import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assume.assumeTrue;
 
@@ -14,7 +13,6 @@ import com.android.tools.r8.D8;
 import com.android.tools.r8.D8TestBuilder;
 import com.android.tools.r8.D8TestCompileResult;
 import com.android.tools.r8.R8;
-import com.android.tools.r8.TestDiagnosticMessages;
 import com.android.tools.r8.TestParameters;
 import com.android.tools.r8.TestParametersCollection;
 import com.android.tools.r8.TestRuntime.CfVm;
@@ -128,18 +126,11 @@ public class HelloWorldCompiledOnArtTest extends APIConversionTestBase {
     if (parameters.getApiLevel().getLevel() < AndroidApiLevel.O.getLevel()) {
       d8TestBuilder.addProgramFiles(getPathBackport());
     }
-    D8TestCompileResult compile =
-        d8TestBuilder
-            .setMinApi(parameters.getApiLevel())
-            .enableCoreLibraryDesugaring(parameters.getApiLevel())
-            .addOptionsModification(opt -> opt.testing.trackDesugaredAPIConversions = true)
-            .compile();
-    TestDiagnosticMessages diagnosticMessages = compile.getDiagnosticMessages();
-    assertTrue(
-        diagnosticMessages.getWarnings().isEmpty()
-            || diagnosticMessages.getWarnings().stream()
-                .noneMatch(x -> x.getDiagnosticMessage().contains("andThen")));
-    return compile
+    return d8TestBuilder
+        .setMinApi(parameters.getApiLevel())
+        .enableCoreLibraryDesugaring(parameters.getApiLevel())
+        .addOptionsModification(opt -> opt.testing.trackDesugaredAPIConversions = true)
+        .compile()
         .assertNoWarningMessageThatMatches(containsString("andThen"))
         .addDesugaredCoreLibraryRunClassPath(
             this::buildDesugaredLibraryWithConversionExtension, parameters.getApiLevel())
