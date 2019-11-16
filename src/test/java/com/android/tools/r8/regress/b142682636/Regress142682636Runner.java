7910f19ab8d52e9969806c1eb99e922480131c50
Jinseong Jeon <jsjeon@google.com>
2019-11-15 21:54:01 -0800

    Add copyright.
    Change-Id: Iaaf95bd10c7ad8034a478f738a8a22edb1d39567

--- src/test/java/com/android/tools/r8/regress/b142682636/Regress142682636Runner.java
+++ src/test/java/com/android/tools/r8/regress/b142682636/Regress142682636Runner.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 package com.android.tools.r8.regress.b142682636;
 
 import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
