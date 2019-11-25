76b999099c706271b442a85282f74d08bc735b87
Christoffer Quist Adamsen <christofferqa@google.com>
2019-11-25 15:56:05 +0100

    Fix missing phi-users check in parameter usages info
    Bug: 145090972
    Change-Id: I1cafb8cce412b0b00205c5e9c86fb63048bd512e

--- src/main/java/com/android/tools/r8/ir/optimize/info/ParameterUsagesInfo.java
+++ src/main/java/com/android/tools/r8/ir/optimize/info/ParameterUsagesInfo.java
@@ -147,8 +147,9 @@ public final class ParameterUsagesInfo {
     // Returns false if the instruction is not supported.
     public boolean note(Instruction instruction) {
       if (instruction.isAssume()) {
-        // Keep examining other users, but the param usage builder should consider aliased users.
-        return true;
+        // Keep examining other users if there are no phi users, but the param usage builder should
+        // consider aliased users.
+        return !instruction.outValue().hasPhiUsers();
       }
       if (instruction.isIf()) {
         return note(instruction.asIf());
