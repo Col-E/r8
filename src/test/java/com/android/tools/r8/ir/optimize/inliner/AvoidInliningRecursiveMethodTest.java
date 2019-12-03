cfa8dc1fad82aa4b4dbc2e71b6217c3aaaebe35c
Christoffer Quist Adamsen <christofferqa@google.com>
2019-12-03 11:11:58 +0100

    Add a regression test for inlining of recursive methods
    Bug: 145276800
    Change-Id: I063c620e4edd7804250cb9166e95240e9556fdc5

---
+++ src/test/java/com/android/tools/r8/ir/optimize/inliner/AvoidInliningRecursiveMethodTest.java
@@ -0,0 +1,79 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.ir.optimize.inliner;
+
+import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.codeinspector.ClassSubject;
+import com.android.tools.r8.utils.codeinspector.CodeInspector;
+import com.android.tools.r8.utils.codeinspector.InstructionSubject;
+import com.android.tools.r8.utils.codeinspector.MethodSubject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class AvoidInliningRecursiveMethodTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  @Parameterized.Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  public AvoidInliningRecursiveMethodTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(AvoidInliningRecursiveMethodTest.class)
+        .addKeepMainRule(TestClass.class)
+        .enableInliningAnnotations()
+        .setMinApi(parameters.getApiLevel())
+        .compile()
+        .inspect(this::inspect)
+        .run(parameters.getRuntime(), TestClass.class)
+        .assertSuccessWithOutputLines("Hello world!");
+  }
+
+  private void inspect(CodeInspector inspector) {
+    ClassSubject classSubject = inspector.clazz(TestClass.class);
+    assertThat(classSubject, isPresent());
+
+    MethodSubject mainMethodSubject = classSubject.mainMethod();
+    assertThat(mainMethodSubject, isPresent());
+
+    // TODO(b/145276800): Should not inline recursive methods.
+    assertTrue(mainMethodSubject.streamInstructions().anyMatch(InstructionSubject::isIf));
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {
+      System.out.println(recursive(getInt()));
+    }
+
+    @NeverInline
+    static int getInt() {
+      return System.currentTimeMillis() >= 0 ? 42 : 0;
+    }
+
+    static String recursive(int x) {
+      if (x > 0) {
+        return recursive(x - 1);
+      }
+      return "Hello world!";
+    }
+  }
+}
