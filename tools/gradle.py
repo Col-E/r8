9794cfd75c93f98aa96261c4269e05b922cddff3
Ian Zerny <zerny@google.com>
2020-02-04 07:57:35 +0100

    Increase VM memory for the gradle daemon.
    This also improved the printing of exec commands for easier copy/paste.
    
    Change-Id: I584644969bc999cdc108b65258b95f248f4adff2

--- tools/gradle.py
+++ tools/gradle.py
@@ -41,6 +41,7 @@ def ParseOptions():
 def GetJavaEnv(env):
   java_env = dict(env if env else os.environ, JAVA_HOME = jdk.GetJdkHome())
   java_env['PATH'] = java_env['PATH'] + os.pathsep + os.path.join(jdk.GetJdkHome(), 'bin')
+  java_env['GRADLE_OPTS'] = '-Xmx1g'
   return java_env
 
 def PrintCmd(s):
