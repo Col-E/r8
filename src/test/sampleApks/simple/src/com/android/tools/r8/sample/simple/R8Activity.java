7197f502771aa5612f872ccb344ce0003b9cb288
Rico Wind <ricow@google.com>
2020-02-07 07:23:25 +0100

    Update simple sample app to use a resource directly from code
    Also, add getting of 10 non-existing resource ids with ids that we
    can mangle with in resource shrinking.
    
    Bug: 148441055
    Change-Id: Ibfd966f575f13fb96251b043d1a395153b73e4a5

--- src/test/sampleApks/simple/src/com/android/tools/r8/sample/simple/R8Activity.java
+++ src/test/sampleApks/simple/src/com/android/tools/r8/sample/simple/R8Activity.java
@@ -7,11 +7,40 @@ package com.android.tools.r8.sample.simple;
 import android.app.Activity;
 import android.os.Bundle;
 import com.android.tools.r8.sample.simple.R;
+import java.util.ArrayList;
+import android.content.res.Resources;
+
 
 public class R8Activity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setTheme(android.R.style.Theme_Light);
     setContentView(R.layout.main);
+    System.out.println(R.string.referenced_from_code);
+    useResources();
+  }
+
+
+  public void useResources() {
+    // Add usage of resource identifiers with ID's that would never
+    // exist in aapt generated R class (so that we can swap them out).
+    ArrayList<Integer> resources = new ArrayList();
+    resources.add(0x7fDEAD01);
+    resources.add(0x7fDEAD02);
+    resources.add(0x7fDEAD03);
+    resources.add(0x7fDEAD04);
+    resources.add(0x7fDEAD05);
+    resources.add(0x7fDEAD06);
+    resources.add(0x7fDEAD07);
+    resources.add(0x7fDEAD08);
+    resources.add(0x7fDEAD09);
+    resources.add(0x7fDEAD0a);
+    for (int id : resources) {
+      try {
+        getResources().getResourceName(id);
+      } catch (Resources.NotFoundException e) {
+        // Do nothing
+      }
+    }
   }
 }
