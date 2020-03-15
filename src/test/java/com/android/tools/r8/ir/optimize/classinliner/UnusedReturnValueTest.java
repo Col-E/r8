8a7cb11fc04118ff7533022a47e445c637f0c559
Christoffer Quist Adamsen <christofferqa@google.com>
2020-03-15 08:27:17 +0100

    Reproduce class inliner bug from unused phi user
    Change-Id: I9ca4ef177818c1eb59773b2980c4fc7d6f6716d4

---
+++ src/test/java/com/android/tools/r8/ir/optimize/classinliner/UnusedReturnValueTest.java
@@ -0,0 +1,69 @@
+// Copyright (c) 2020, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.ir.optimize.classinliner;
+
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class UnusedReturnValueTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  @Parameterized.Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  public UnusedReturnValueTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(UnusedReturnValueTest.class)
+        .addKeepMainRule(TestClass.class)
+        // TODO(christofferqa): Fix class inliner.
+        .allowClassInlinerGracefulExit()
+        .enableInliningAnnotations()
+        .setMinApi(parameters.getApiLevel())
+        .compile()
+        .run(parameters.getRuntime(), TestClass.class)
+        .assertSuccessWithOutputLines("Hello world!");
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {
+      Builder builder = new Builder();
+      helper(builder);
+      System.out.println(builder.build());
+    }
+
+    @NeverInline
+    static Builder helper(Builder builder) {
+      builder.message = "Hello world!";
+      if (System.currentTimeMillis() > 0) {
+        return builder;
+      }
+      return null;
+    }
+  }
+
+  static class Builder {
+
+    String message;
+
+    String build() {
+      return message;
+    }
+  }
+}
