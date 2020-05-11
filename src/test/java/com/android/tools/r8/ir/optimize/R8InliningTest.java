17e16344121514c70058a234439de7818d91d1a6
Christoffer Quist Adamsen <christofferqa@google.com>
2020-05-11 20:13:57 +0200

    Update test expectation after disabling enum unboxing for CF
    Change-Id: Iba1f5612289c01bf2b402c18763a6907345a530c

--- src/test/java/com/android/tools/r8/ir/optimize/R8InliningTest.java
+++ src/test/java/com/android/tools/r8/ir/optimize/R8InliningTest.java
@@ -379,6 +379,6 @@ public class R8InliningTest extends TestBase {
       }
     }
     assertEquals(1, instanceGetCount);
-    assertEquals(0, invokeCount);
+    assertEquals(BooleanUtils.intValue(parameters.isCfRuntime()), invokeCount);
   }
 }
