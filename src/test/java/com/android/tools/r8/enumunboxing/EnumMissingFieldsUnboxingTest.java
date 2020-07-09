d2e9bcb066f0a3ab42b0c7897ba57e835e6d94af
Clément Béra <clementbera@google.com>
2020-07-09 11:21:34 +0000

    Enum unboxing: add test for missing case
    Change-Id: If603a47fe3795945098475991a6036de0aa34032

---
+++ src/test/java/com/android/tools/r8/enumunboxing/EnumMissingFieldsUnboxingTest.java
@@ -0,0 +1,90 @@
+// Copyright (c) 2020, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.enumunboxing;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.R8TestRunResult;
+import com.android.tools.r8.TestParameters;
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class EnumMissingFieldsUnboxingTest extends EnumUnboxingTestBase {
+
+  private final TestParameters parameters;
+  private final boolean enumValueOptimization;
+  private final EnumKeepRules enumKeepRules;
+
+  @Parameters(name = "{0} valueOpt: {1} keep: {2}")
+  public static List<Object[]> data() {
+    return enumUnboxingTestParameters();
+  }
+
+  public EnumMissingFieldsUnboxingTest(
+      TestParameters parameters, boolean enumValueOptimization, EnumKeepRules enumKeepRules) {
+    this.parameters = parameters;
+    this.enumValueOptimization = enumValueOptimization;
+    this.enumKeepRules = enumKeepRules;
+  }
+
+  @Test
+  public void testEnumUnboxing() throws Exception {
+    R8TestRunResult run =
+        testForR8(parameters.getBackend())
+            .addProgramClasses(TestClass.class)
+            .addProgramClassFileData(getEnumProgramData())
+            .addKeepMainRule(TestClass.class)
+            .addKeepRules(enumKeepRules.getKeepRules())
+            .enableNeverClassInliningAnnotations()
+            .addOptionsModification(opt -> enableEnumOptions(opt, enumValueOptimization))
+            .allowDiagnosticInfoMessages()
+            .setMinApi(parameters.getApiLevel())
+            .compile()
+            .inspectDiagnosticMessages(
+                m -> assertEnumIsBoxed(CompilationEnum.class, TestClass.class.getSimpleName(), m))
+            .run(parameters.getRuntime(), TestClass.class)
+            .assertFailureWithErrorThatMatches(containsString("NoSuchFieldError"));
+    assertLines2By2Correct(run.getStdOut());
+  }
+
+  private byte[] getEnumProgramData() throws IOException {
+    return transformer(RuntimeEnum.class)
+        .setClassDescriptor(descriptor(CompilationEnum.class))
+        .transform();
+  }
+
+  // CompilationEnum is used for the compilation of TestClass.
+  @NeverClassInline
+  public enum CompilationEnum {
+    A,
+    B,
+    C,
+    D
+  }
+
+  // CompilationEnum is used for the runtime execution of TestClass.
+  @NeverClassInline
+  public enum RuntimeEnum {
+    A,
+    D
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {
+      System.out.println(CompilationEnum.A.ordinal());
+      System.out.println(0);
+      // The field C will be missing at runtime.
+      System.out.println(CompilationEnum.C.ordinal());
+      System.out.println(2);
+    }
+  }
+}
