bb22f711660a8ea67a06c53c37a933a8a5479661
Søren Gjesse <sgjesse@google.com>
2020-09-29 14:29:08 +0200

    Reapply "Use ReferenceTrace to collect keep rules for library desugar test"
    This reverts commit edf8439d2bc78bfc7e9b6a5052edffda49bcaba3 and fixes
    tracereferences when the trace source is DEX code.
    
    Bug: 169127026
    Change-Id: I984b561461763d7c0e4aa7490b93ab37a245afb5

--- src/main/java/com/android/tools/r8/tracereferences/TraceReferences.java
+++ src/main/java/com/android/tools/r8/tracereferences/TraceReferences.java
@@ -3,16 +3,22 @@
 // BSD-style license that can be found in the LICENSE file.
 package com.android.tools.r8.tracereferences;
 
-import static com.android.tools.r8.utils.ExceptionUtils.STATUS_ERROR;
-
 import com.android.tools.r8.CompilationFailedException;
 import com.android.tools.r8.Keep;
 import com.android.tools.r8.ProgramResource;
+import com.android.tools.r8.ProgramResource.Kind;
 import com.android.tools.r8.ProgramResourceProvider;
 import com.android.tools.r8.Version;
+import com.android.tools.r8.dex.ApplicationReader;
+import com.android.tools.r8.graph.DexProgramClass;
 import com.android.tools.r8.origin.Origin;
 import com.android.tools.r8.utils.AndroidApp;
 import com.android.tools.r8.utils.ExceptionDiagnostic;
+import com.android.tools.r8.utils.ExceptionUtils;
+import com.android.tools.r8.utils.InternalOptions;
+import com.android.tools.r8.utils.StringDiagnostic;
+import com.android.tools.r8.utils.Timing;
+import com.google.common.collect.ImmutableList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -53,7 +59,25 @@ public class TraceReferences {
         .forEach(provider -> tagetDescriptors.addAll(provider.getClassDescriptors()));
     for (ProgramResourceProvider provider : command.getSource()) {
       for (ProgramResource programResource : provider.getProgramResources()) {
-        tagetDescriptors.removeAll(programResource.getClassDescriptors());
+        if (programResource.getKind() == Kind.DEX) {
+          command
+              .getDiagnosticsHandler()
+              .warning(new StringDiagnostic("DEX files not fully supported"));
+          assert programResource.getClassDescriptors() == null;
+          for (DexProgramClass clazz :
+              new ApplicationReader(
+                      AndroidApp.builder()
+                          .addDexProgramData(ImmutableList.of(programResource.getBytes()))
+                          .build(),
+                      new InternalOptions(),
+                      Timing.empty())
+                  .read()
+                  .classes()) {
+            tagetDescriptors.remove(clazz.getType().toDescriptorString());
+          }
+        } else {
+          tagetDescriptors.removeAll(programResource.getClassDescriptors());
+        }
       }
     }
     Tracer tracer = new Tracer(tagetDescriptors, builder.build());
@@ -77,25 +101,20 @@ public class TraceReferences {
     formatter.format(result);
   }
 
-  public static void main(String... args) {
-    try {
-      TraceReferencesCommand command = TraceReferencesCommand.parse(args, Origin.root()).build();
-      if (command.isPrintHelp()) {
-        System.out.println(TraceReferencesCommandParser.USAGE_MESSAGE);
-        return;
-      }
-      if (command.isPrintVersion()) {
-        System.out.println("referencetrace " + Version.getVersionString());
-        return;
-      }
-      run(command);
-    } catch (CompilationFailedException e) {
-      System.exit(STATUS_ERROR);
-    } catch (Throwable e) {
-      System.err.println("ReferenceTrace failed with an internal error.");
-      Throwable cause = e.getCause() == null ? e : e.getCause();
-      cause.printStackTrace();
-      System.exit(STATUS_ERROR);
+  public static void run(String... args) throws CompilationFailedException {
+    TraceReferencesCommand command = TraceReferencesCommand.parse(args, Origin.root()).build();
+    if (command.isPrintHelp()) {
+      System.out.println(TraceReferencesCommandParser.USAGE_MESSAGE);
+      return;
     }
+    if (command.isPrintVersion()) {
+      System.out.println("referencetrace " + Version.getVersionString());
+      return;
+    }
+    run(command);
+  }
+
+  public static void main(String[] args) {
+    ExceptionUtils.withMainProgramHandler(() -> run(args));
   }
 }
