a540f45cae40220ffffea2b780592e011b52fe30
Morten Krogh-Jespersen <mkroghj@google.com>
2020-10-05 11:08:47 +0200

    Migrate legacy MinifierFieldSignatureTest
    Bug: 167145686
    Change-Id: I3d98fb0ac528f314387b4900a94d90d2daf9cf1b

--- src/test/java/com/android/tools/r8/naming/MinifierFieldSignatureTest.java
+++ src/test/java/com/android/tools/r8/naming/MinifierFieldSignatureTest.java
@@ -4,8 +4,11 @@
 
 package com.android.tools.r8.naming;
 
+import static com.android.tools.r8.DiagnosticsMatcher.diagnosticMessage;
 import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
 import static com.android.tools.r8.utils.codeinspector.Matchers.isPresentAndRenamed;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
@@ -21,16 +24,16 @@ import static org.objectweb.asm.Opcodes.PUTFIELD;
 import static org.objectweb.asm.Opcodes.RETURN;
 import static org.objectweb.asm.Opcodes.V1_8;
 
-import com.android.tools.r8.DiagnosticsChecker;
-import com.android.tools.r8.R8Command;
-import com.android.tools.r8.StringConsumer;
+import com.android.tools.r8.R8TestCompileResult;
 import com.android.tools.r8.TestBase;
-import com.android.tools.r8.ToolHelper;
+import com.android.tools.r8.TestDiagnosticMessages;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
 import com.android.tools.r8.origin.Origin;
+import com.android.tools.r8.shaking.ProguardKeepAttributes;
 import com.android.tools.r8.utils.codeinspector.ClassSubject;
 import com.android.tools.r8.utils.codeinspector.CodeInspector;
 import com.android.tools.r8.utils.codeinspector.FieldSubject;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -44,6 +47,18 @@ import org.objectweb.asm.MethodVisitor;
 
 @RunWith(Parameterized.class)
 public class MinifierFieldSignatureTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  public MinifierFieldSignatureTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
   /*
 
   class Fields<X extends String> {
@@ -61,16 +76,6 @@ public class MinifierFieldSignatureTest extends TestBase {
   private String anArrayOfXSignature = "[TX;";
   private String aFieldsOfXSignature = "LFields<TX;>;";
   private String aFieldsOfXInnerSignature = "LFields<TX;>.Inner;";
-  private Backend backend;
-
-  @Parameters(name = "Backend: {0}")
-  public static Backend[] data() {
-    return ToolHelper.getBackends();
-  }
-
-  public MinifierFieldSignatureTest(Backend backend) {
-    this.backend = backend;
-  }
 
   public byte[] dumpFields(Map<String, String> signatures) throws Exception {
 
@@ -172,25 +177,23 @@ public class MinifierFieldSignatureTest extends TestBase {
 
   public void runTest(
       ImmutableMap<String, String> signatures,
-      Consumer<DiagnosticsChecker> diagnostics,
+      Consumer<TestDiagnosticMessages> diagnostics,
       Consumer<CodeInspector> inspect)
       throws Exception {
-    DiagnosticsChecker checker = new DiagnosticsChecker();
-    CodeInspector inspector =
-        new CodeInspector(
-            ToolHelper.runR8(
-                R8Command.builder(checker)
-                    .addClassProgramData(dumpFields(signatures), Origin.unknown())
-                    .addClassProgramData(dumpInner(), Origin.unknown())
-                    .addProguardConfiguration(
-                        ImmutableList.of(
-                            "-keepattributes InnerClasses,EnclosingMethod,Signature",
-                            "-keep,allowobfuscation class ** { *; }"),
-                        Origin.unknown())
-                    .setProgramConsumer(emptyConsumer(backend))
-                    .addLibraryFiles(runtimeJar(backend))
-                    .setProguardMapConsumer(StringConsumer.emptyConsumer())
-                    .build()));
+    R8TestCompileResult compileResult =
+        testForR8(parameters.getBackend())
+            .addProgramClassFileData(dumpFields(signatures), dumpInner())
+            .addKeepAttributes(
+                ProguardKeepAttributes.INNER_CLASSES,
+                ProguardKeepAttributes.ENCLOSING_METHOD,
+                ProguardKeepAttributes.SIGNATURE)
+            .addKeepAllClassesRuleWithAllowObfuscation()
+            .setMinApi(parameters.getApiLevel())
+            .allowDiagnosticMessages()
+            .compile();
+
+    CodeInspector inspector = compileResult.inspector();
+
     // All classes are kept, and renamed.
     ClassSubject clazz = inspector.clazz("Fields");
     assertThat(clazz, isPresentAndRenamed());
@@ -225,13 +228,18 @@ public class MinifierFieldSignatureTest extends TestBase {
       assertEquals(
           aFieldsOfXInnerSignature, aFieldsOfXInner.getOriginalSignatureAttribute());
     }
-
-    diagnostics.accept(checker);
     inspect.accept(inspector);
+
+    TestDiagnosticMessages diagnosticMessages = compileResult.getDiagnosticMessages();
+    diagnosticMessages.assertNoInfos();
+    diagnosticMessages.assertNoErrors();
+    diagnostics.accept(diagnosticMessages);
   }
 
-  private void testSingleField(String name, String signature,
-      Consumer<DiagnosticsChecker> diagnostics,
+  private void testSingleField(
+      String name,
+      String signature,
+      Consumer<TestDiagnosticMessages> diagnostics,
       Consumer<CodeInspector> inspector)
       throws Exception {
     ImmutableMap<String, String> signatures = ImmutableMap.of(name, signature);
@@ -242,11 +250,8 @@ public class MinifierFieldSignatureTest extends TestBase {
     assertSame(Origin.unknown(), origin);
   }
 
-  private void noWarnings(DiagnosticsChecker checker) {
-    assertEquals(0, checker.warnings.size());
-  }
-
   private void noInspection(CodeInspector inspector) {
+    // Intentionally left empty.
   }
 
   private void noSignatureAttribute(FieldSubject field) {
@@ -258,48 +263,62 @@ public class MinifierFieldSignatureTest extends TestBase {
   @Test
   public void originalJavacSignatures() throws Exception {
     // Test using the signatures generated by javac.
-    runTest(ImmutableMap.of(), this::noWarnings, this::noInspection);
+    runTest(ImmutableMap.of(), TestDiagnosticMessages::assertNoWarnings, this::noInspection);
   }
 
   @Test
   public void signatureEmpty() throws Exception {
-    testSingleField("anX", "", this::noWarnings,
+    testSingleField(
+        "anX",
+        "",
+        TestDiagnosticMessages::assertNoWarnings,
         inspector -> noSignatureAttribute(lookupAnX(inspector)));
   }
 
   @Test
   public void signatureInvalid() throws Exception {
-    testSingleField("anX", "X", diagnostics -> {
-      assertEquals(1, diagnostics.warnings.size());
-      // TODO(sgjesse): The position 2 reported here is one off.
-      DiagnosticsChecker.checkDiagnostic(diagnostics.warnings.get(0), this::isOriginUnknown,
-          "Invalid signature 'X' for field",
-          "java.lang.String Fields.anX",
-          "Expected L, [ or T at position 2");
-    }, inspector -> noSignatureAttribute(lookupAnX(inspector)));
+    testSingleField(
+        "anX",
+        "X",
+        diagnostics -> {
+          diagnostics.assertWarningsMatch(
+              diagnosticMessage(
+                  allOf(
+                      containsString("Invalid signature 'X' for field"),
+                      containsString("java.lang.String Fields.anX"),
+                      // TODO(sgjesse): The position 2 reported here is one off.
+                      containsString("Expected L, [ or T at position 2"))));
+        },
+        inspector -> noSignatureAttribute(lookupAnX(inspector)));
   }
 
   @Test
   public void classNotFound() throws Exception {
     String signature = "LNotFound<TX;>.InnerNotFound.InnerAlsoNotFound;";
-    testSingleField("anX", signature, this::noWarnings, inspector -> {
-      assertThat(inspector.clazz("NotFound"), not(isPresent()));
-      assertEquals(signature, lookupAnX(inspector).getOriginalSignatureAttribute());
-    });
+    testSingleField(
+        "anX",
+        signature,
+        TestDiagnosticMessages::assertNoWarnings,
+        inspector -> {
+          assertThat(inspector.clazz("NotFound"), not(isPresent()));
+          assertEquals(signature, lookupAnX(inspector).getOriginalSignatureAttribute());
+        });
   }
 
   @Test
   public void multipleWarnings() throws Exception {
-    runTest(ImmutableMap.of(
-        "anX", "X",
-        "anArrayOfX", "X",
-        "aFieldsOfX", "X"
-    ), diagnostics -> {
-      assertEquals(3, diagnostics.warnings.size());
-    }, inspector -> {
-      noSignatureAttribute(lookupAnX(inspector));
-      noSignatureAttribute(lookupAnArrayOfX(inspector));
-      noSignatureAttribute(lookupAFieldsOfX(inspector));
-    });
+    runTest(
+        ImmutableMap.of(
+            "anX", "X",
+            "anArrayOfX", "X",
+            "aFieldsOfX", "X"),
+        diagnostics -> {
+          diagnostics.assertWarningsCount(3);
+        },
+        inspector -> {
+          noSignatureAttribute(lookupAnX(inspector));
+          noSignatureAttribute(lookupAnArrayOfX(inspector));
+          noSignatureAttribute(lookupAFieldsOfX(inspector));
+        });
   }
 }
