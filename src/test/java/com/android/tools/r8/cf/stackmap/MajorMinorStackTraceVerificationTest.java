eea481e5bc64bdd64e1977a36ab9dfa5ed84940b
Morten Krogh-Jespersen <mkroghj@google.com>
2020-10-14 16:18:14 +0200

    Test for major-minor version with stackmap verification
    Bug: 169918924
    Change-Id: I50fd1db8d8a8f097e8028916d9dbe90993aacf9c

---
+++ src/test/java/com/android/tools/r8/cf/stackmap/MajorMinorStackTraceVerificationTest.java
@@ -0,0 +1,51 @@
+// Copyright (c) 2020, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.cf.stackmap;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.cf.CfVersion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class MajorMinorStackTraceVerificationTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  public MajorMinorStackTraceVerificationTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    testForR8(parameters.getBackend())
+        .addProgramClassFileData(
+            transformer(Main.class).setVersion(CfVersion.V1_1).stripFrames("main").transform())
+        .setMinApi(parameters.getApiLevel())
+        .addKeepMainRule(Main.class)
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("Hello World");
+  }
+
+  public static class Main {
+
+    public static void main(String[] args) {
+      if (args.length == 0) {
+        System.out.println("Hello World");
+      } else {
+        System.out.println("World, Hello");
+      }
+    }
+  }
+}
