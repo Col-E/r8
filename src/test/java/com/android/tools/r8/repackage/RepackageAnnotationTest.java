7d85c1053217f6f5c6df0db6dd96c99b4fdb091e
Morten Krogh-Jespersen <mkroghj@google.com>
2020-11-11 17:21:25 +0100

    Add annotation repackage test
    Bug: 173004917
    Change-Id: Ie1a85cdfd86abf1bcf250b257827f99cf770406c

---
+++ src/test/java/com/android/tools/r8/repackage/RepackageAnnotationTest.java
@@ -0,0 +1,82 @@
+// Copyright (c) 2020, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.repackage;
+
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestParameters;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class RepackageAnnotationTest extends RepackageTestBase {
+
+  public RepackageAnnotationTest(
+      String flattenPackageHierarchyOrRepackageClasses, TestParameters parameters) {
+    super(flattenPackageHierarchyOrRepackageClasses, parameters);
+  }
+
+  @Test
+  public void testRuntime() throws Exception {
+    testForRuntime(parameters)
+        .addProgramClasses(Main.class, Annotation.class, A.class)
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("Hello World");
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    testForR8(parameters.getBackend())
+        .addProgramClasses(Main.class, A.class, Annotation.class)
+        .setMinApi(parameters.getApiLevel())
+        .addKeepMainRule(Main.class)
+        .enableInliningAnnotations()
+        .enableNeverClassInliningAnnotations()
+        .addKeepRuntimeVisibleAnnotations()
+        .addKeepRules(
+            "-keep,allowobfuscation @interface " + Annotation.class.getTypeName() + " {",
+            "  *;",
+            "}")
+        .apply(this::configureRepackaging)
+        .run(parameters.getRuntime(), Main.class)
+        // TODO(b/173004917): Should be Hello World.
+        .assertSuccessWithOutputLines("null");
+  }
+
+  public static class Main {
+
+    public static void main(String[] args) {
+      System.out.println(new A().getAnnotationValues());
+    }
+  }
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target({ElementType.TYPE})
+  public @interface Annotation {
+
+    String f1();
+
+    String f2();
+  }
+
+  @Annotation(f1 = "Hello", f2 = "World")
+  @NeverClassInline
+  public static class A {
+
+    @NeverInline
+    public String getAnnotationValues() {
+      Annotation annotation = A.class.getAnnotation(Annotation.class);
+      if (annotation == null) {
+        return null;
+      }
+      return annotation.f1() + " " + annotation.f2();
+    }
+  }
+}
