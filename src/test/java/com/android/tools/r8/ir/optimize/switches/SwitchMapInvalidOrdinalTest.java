ded879894a0ca45e72d35ee98584aa78f9e9da46
Morten Krogh-Jespersen <mkroghj@google.com>
2020-11-24 16:46:46 +0100

    Suppress unchecked cast warning in SwitchMapInvalidOrdinalTest
    Change-Id: I368f24dcd6745ee3e7b4f98b4516a1ed5db006b9

--- src/test/java/com/android/tools/r8/ir/optimize/switches/SwitchMapInvalidOrdinalTest.java
+++ src/test/java/com/android/tools/r8/ir/optimize/switches/SwitchMapInvalidOrdinalTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+@SuppressWarnings("unchecked")
 @RunWith(Parameterized.class)
 public class SwitchMapInvalidOrdinalTest extends TestBase {
   private final TestParameters parameters;
