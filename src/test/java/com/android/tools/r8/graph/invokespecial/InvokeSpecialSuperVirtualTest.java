3b704e8071c958e592d56dc99cbdfeec360a8d17
Clément Béra <clementbera@google.com>
2020-12-10 08:40:01 +0000

    Invoke special super virtual test
    Bug: 110175213
    Change-Id: Ib1f734a0bba621fd830851ac1d5b74a55b6bf81f

---
+++ src/test/java/com/android/tools/r8/graph/invokespecial/InvokeSpecialSuperVirtualTest.java
@@ -0,0 +1,144 @@
+// Copyright (c) 2019, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.graph.invokespecial;
+
+import static com.android.tools.r8.DiagnosticsMatcher.diagnosticMessage;
+import static com.android.tools.r8.utils.DescriptorUtils.getBinaryNameFromJavaType;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.objectweb.asm.Opcodes.INVOKESPECIAL;
+
+import com.android.tools.r8.CompilationFailedException;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.StringUtils;
+import java.io.IOException;
+import org.junit.Assume;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class InvokeSpecialSuperVirtualTest extends TestBase {
+
+  private static final String EXPECTED_RESULT =
+      StringUtils.lines("A", "B", "A", "C", "A", "B", "A", "B", "A");
+
+  private final TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  public InvokeSpecialSuperVirtualTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void testJVM() throws Exception {
+    Assume.assumeTrue(parameters.isCfRuntime());
+    testForJvm()
+        .addProgramClasses(A.class, C.class, Main.class)
+        .addProgramClassFileData(getClassBWithTransformedInvoked())
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutput(EXPECTED_RESULT);
+  }
+
+  @Test(expected = CompilationFailedException.class)
+  public void testD8() throws Exception {
+    Assume.assumeTrue(parameters.isDexRuntime());
+    testForD8()
+        .addProgramClasses(A.class, C.class, Main.class)
+        .addProgramClassFileData(getClassBWithTransformedInvoked())
+        .setMinApi(parameters.getApiLevel())
+        // TODO(b/110175213): Should succeed with the expected result.
+        .compileWithExpectedDiagnostics(
+            diagnostics ->
+                diagnostics
+                    .assertOnlyErrors()
+                    .assertErrorsMatch(
+                        diagnosticMessage(
+                            containsString("Failed to compile unsupported use of invokespecial"))));
+  }
+
+  @Test(expected = CompilationFailedException.class)
+  public void testR8() throws Exception {
+    testForR8(parameters.getBackend())
+        .addProgramClasses(A.class, C.class, Main.class)
+        .addProgramClassFileData(getClassBWithTransformedInvoked())
+        .addKeepMainRule(Main.class)
+        .setMinApi(parameters.getApiLevel())
+        // TODO(b/110175213): Should succeed with the expected result.
+        .compileWithExpectedDiagnostics(
+            diagnostics ->
+                diagnostics
+                    .assertOnlyErrors()
+                    .assertErrorsMatch(
+                        diagnosticMessage(
+                            containsString("Failed to compile unsupported use of invokespecial"))));
+  }
+
+  private byte[] getClassBWithTransformedInvoked() throws IOException {
+    return transformer(B.class)
+        .transformMethodInsnInMethod(
+            "printAll",
+            (opcode, owner, name, descriptor, isInterface, continuation) -> {
+              if (name.equals("replace")) {
+                assertEquals(INVOKESPECIAL, opcode);
+                assertEquals(getBinaryNameFromJavaType(B.class.getTypeName()), owner);
+                continuation.visitMethodInsn(opcode, owner, "print", descriptor, isInterface);
+              } else {
+                continuation.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+              }
+            })
+        .transform();
+  }
+
+  public static class A {
+
+    void print() {
+      System.out.println("A");
+    }
+  }
+
+  public static class B extends A {
+
+    void print() {
+      System.out.println("B");
+      super.print();
+    }
+
+    private void replace() {
+      System.out.println("Should not be called.");
+    }
+
+    void printAll() {
+      // Invoke-super.
+      super.print();
+      // Rewritten to Invoke-special to B::print.
+      replace();
+      // Invoke-virtual.
+      print();
+    }
+  }
+
+  public static class C extends B {
+
+    void print() {
+      System.out.println("C");
+    }
+  }
+
+  public static class Main {
+
+    public static void main(String[] args) {
+      new C().printAll();
+      new B().printAll();
+    }
+  }
+}
