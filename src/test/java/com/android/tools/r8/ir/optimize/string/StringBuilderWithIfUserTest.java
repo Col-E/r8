da118cf494e337c95432396dc5d0ab861def272c
Christoffer Quist Adamsen <christofferqa@google.com>
2021-01-06 08:23:54 +0100

    Reproduce unexpected if in string builder optimization
    Change-Id: I53f2c2730567bd3878f776bbe7bcba04dda18533

---
+++ src/test/java/com/android/tools/r8/ir/optimize/string/StringBuilderWithIfUserTest.java
@@ -0,0 +1,55 @@
+// Copyright (c) 2021, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.ir.optimize.string;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.codeinspector.AssertUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class StringBuilderWithIfUserTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  @Parameterized.Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  public StringBuilderWithIfUserTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void test() throws Exception {
+    AssertUtils.assertFailsCompilationIf(
+        parameters.isDexRuntime(),
+        () -> {
+          testForR8(parameters.getBackend())
+              .addInnerClasses(StringBuilderWithIfUserTest.class)
+              .addKeepMainRule(Main.class)
+              .setMinApi(parameters.getApiLevel())
+              .compile()
+              .run(parameters.getRuntime(), Main.class)
+              .assertSuccessWithOutputLines("Hello world!");
+        });
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      StringBuilder builder = new StringBuilder();
+      StringBuilder other = System.currentTimeMillis() > 0 ? new StringBuilder() : builder;
+      builder.append("Hello world!");
+      if (builder != other) {
+        System.out.println(builder.toString());
+      }
+    }
+  }
+}
