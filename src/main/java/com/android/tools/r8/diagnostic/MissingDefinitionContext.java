7ac5ad6cbf3f83955517703cd6d2967ca6c13167
Christoffer Quist Adamsen <christofferqa@google.com>
2021-02-15 15:52:03 +0100

    Add accessor for missing definition and context in MissingDefinitionDiagnostic
    Fixes: 180097475
    Change-Id: Id84d419a5d0bf18ae363c10a2beb633680461f1b

--- src/main/java/com/android/tools/r8/diagnostic/MissingDefinitionContext.java
+++ src/main/java/com/android/tools/r8/diagnostic/MissingDefinitionContext.java
@@ -7,6 +7,9 @@ package com.android.tools.r8.diagnostic;
 import com.android.tools.r8.Keep;
 import com.android.tools.r8.origin.Origin;
 import com.android.tools.r8.references.ClassReference;
+import com.android.tools.r8.references.FieldReference;
+import com.android.tools.r8.references.MethodReference;
+import java.util.function.Consumer;
 
 /** A context that references a missing definition in the program, classpath, or library. */
 @Keep
@@ -17,4 +20,14 @@ public interface MissingDefinitionContext {
 
   /** The origin of the context. */
   Origin getOrigin();
+
+  /**
+   * Provides the context in which the missing definition is referenced to {@param
+   * classReferenceConsumer} if the context is a class, to {@param fieldReferenceConsumer} if the
+   * context is a field, and to {@param methodReferenceConsumer} if the context is a method..
+   */
+  void getReference(
+      Consumer<ClassReference> classReferenceConsumer,
+      Consumer<FieldReference> fieldReferenceConsumer,
+      Consumer<MethodReference> methodReferenceConsumer);
 }
--- src/main/java/com/android/tools/r8/diagnostic/MissingDefinitionInfo.java
+++ src/main/java/com/android/tools/r8/diagnostic/MissingDefinitionInfo.java
@@ -5,13 +5,27 @@
 package com.android.tools.r8.diagnostic;
 
 import com.android.tools.r8.Keep;
+import com.android.tools.r8.references.ClassReference;
+import com.android.tools.r8.references.FieldReference;
+import com.android.tools.r8.references.MethodReference;
 import java.util.Collection;
+import java.util.function.Consumer;
 
 /**
  * Information about the contexts that references an item that was not part of the compilation unit.
  */
 @Keep
 public interface MissingDefinitionInfo {
+
+  /**
+   * Provides the missing definition to {@param classReferenceConsumer} if the missing definition is
+   * a class, to {@param fieldReferenceConsumer} if the missing definition is a field, and to
+   * {@param methodReferenceConsumer} if the missing definition is a method..
+   */
+  void getMissingDefinition(
+      Consumer<ClassReference> classReferenceConsumer,
+      Consumer<FieldReference> fieldReferenceConsumer,
+      Consumer<MethodReference> methodReferenceConsumer);
 
   /** The contexts from which this missing definition was referenced. */
   Collection<MissingDefinitionContext> getReferencedFromContexts();
