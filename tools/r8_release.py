7b151115b9343563098a2794d74bc4167e87b57c
Søren Gjesse <sgjesse@google.com>
2021-02-16 14:32:32 +0100

    Add missing bytes to string conversion in tools/r8_release.py
    Change-Id: If748fc35a3a4678ded2064d08bdcb8ea00a8d794

--- tools/r8_release.py
+++ tools/r8_release.py
@@ -577,7 +577,7 @@ def gmaven_publisher_stage(args, gfiles):
   # [INFO] 06/19/2020 09:35:12 CEST: Release ID = 9171d015-18f6-4a90-9984-1c362589dc1b
   # [INFO] 06/19/2020 09:35:12 CEST: Stage Path = /bigstore/studio_staging/maven2/sgjesse/9171d015-18f6-4a90-9984-1c362589dc1b
 
-  matches = GMAVEN_PUBLISH_STAGE_RELEASE_ID_PATTERN.findall(output)
+  matches = GMAVEN_PUBLISH_STAGE_RELEASE_ID_PATTERN.findall(output.decode("utf-8"))
   if matches == None or len(matches) > 1:
     print("Could not determine the release ID from the gmaven_publisher " +
           "output. Expected a line with 'Release ID = <release id>'.")
