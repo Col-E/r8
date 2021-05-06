d65fdf1df517ad8f1bc44d4db335012f7de3019b
Christoffer Quist Adamsen <christofferqa@google.com>
2021-05-06 10:58:30 +0200

    Test assumenosideeffects rule on throwing method
    Bug: 187265955
    Change-Id: I83abd21ddee69326663b726852a97f79ea0fe82e

---
+++ src/test/java/com/android/tools/r8/shaking/assumenosideeffects/AssumeNoSideEffectsRuleOnThrowingMethodTest.java
@@ -0,0 +1,51 @@
+// Copyright (c) 2021, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.shaking.assumenosideeffects;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class AssumeNoSideEffectsRuleOnThrowingMethodTest extends TestBase {
+
+  private final TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  public AssumeNoSideEffectsRuleOnThrowingMethodTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addProgramClasses(Main.class)
+        .addKeepMainRule(Main.class)
+        .addKeepRules("-assumenosideeffects class " + Main.class.getTypeName() + " { ouch(); }")
+        .setMinApi(parameters.getApiLevel())
+        .compile()
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("Hello world!");
+  }
+
+  static class Main {
+    public static void main(String[] args) {
+      ouch();
+      System.out.println("Hello world!");
+    }
+
+    static void ouch() {
+      throw new RuntimeException();
+    }
+  }
+}
