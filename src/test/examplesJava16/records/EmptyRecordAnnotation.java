5b18913069c1206ddd2d21279e6bcd5d42cb5dde
Clément Béra <clementbera@google.com>
2021-09-16 10:27:30 +0000

    Test Record and annotations
    Bug: 199359249
    Change-Id: I2d86a773cf22a2b4e1680d2c962331a5d5e7a461

---
+++ src/test/examplesJava16/records/EmptyRecordAnnotation.java
@@ -0,0 +1,39 @@
+// Copyright (c) 2020, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package records;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+public class EmptyRecordAnnotation {
+
+  record Empty() {}
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @interface ClassAnnotation {
+    Class<? extends Record> theClass();
+  }
+
+  @ClassAnnotation(theClass = Record.class)
+  public static void annotatedMethod1() {}
+
+  @ClassAnnotation(theClass = Empty.class)
+  public static void annotatedMethod2() {}
+
+  public static void main(String[] args) throws Exception {
+    Class<?> annotatedMethod1Content =
+        EmptyRecordAnnotation.class
+            .getDeclaredMethod("annotatedMethod1")
+            .getAnnotation(ClassAnnotation.class)
+            .theClass();
+    System.out.println(annotatedMethod1Content);
+    Class<?> annotatedMethod2Content =
+        EmptyRecordAnnotation.class
+            .getDeclaredMethod("annotatedMethod2")
+            .getAnnotation(ClassAnnotation.class)
+            .theClass();
+    System.out.println(annotatedMethod2Content);
+  }
+}
