a52e827fdb8af12e74d54c92a366313660887542
Ian Zerny <zerny@google.com>
2021-10-07 11:17:12 +0200

    Remove unneeded and incorrect set-file modification.
    Bug: 202254240
    Change-Id: Ic061056039ae67fdb1c8812f6f06d339c5ae329d

--- src/main/java/com/android/tools/r8/naming/SourceFileRewriter.java
+++ src/main/java/com/android/tools/r8/naming/SourceFileRewriter.java
@@ -4,15 +4,10 @@
 package com.android.tools.r8.naming;
 
 import com.android.tools.r8.graph.AppView;
-import com.android.tools.r8.graph.Code;
 import com.android.tools.r8.graph.DexApplication;
 import com.android.tools.r8.graph.DexClass;
-import com.android.tools.r8.graph.DexDebugEvent;
-import com.android.tools.r8.graph.DexDebugEvent.SetFile;
-import com.android.tools.r8.graph.DexDebugInfo;
 import com.android.tools.r8.graph.DexString;
 import com.android.tools.r8.shaking.ProguardConfiguration;
-import java.util.Arrays;
 
 /**
  * Visit program {@link DexClass}es and replace their sourceFile with the given string.
@@ -48,30 +43,6 @@ public class SourceFileRewriter {
         continue;
       }
       clazz.sourceFile = defaultRenaming;
-      clazz.forEachMethod(encodedMethod -> {
-        // Abstract methods do not have code_item.
-        if (encodedMethod.shouldNotHaveCode()) {
-          return;
-        }
-        Code code = encodedMethod.getCode();
-        if (code == null) {
-          return;
-        }
-        if (code.isDexCode()) {
-          DexDebugInfo dexDebugInfo = code.asDexCode().getDebugInfo();
-          if (dexDebugInfo == null) {
-            return;
-          }
-          // Thanks to a single global source file, we can safely remove DBG_SET_FILE entirely.
-          dexDebugInfo.events =
-              Arrays.stream(dexDebugInfo.events)
-                  .filter(dexDebugEvent -> !(dexDebugEvent instanceof SetFile))
-                  .toArray(DexDebugEvent[]::new);
-        } else {
-          assert code.isCfCode();
-          // CF has nothing equivalent to SetFile, so there is nothing to remove.
-        }
-      });
     }
   }
 
