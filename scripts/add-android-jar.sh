29c8c5b4d16a2c8a788c8936fa9e77b32d8682c2
Søren Gjesse <sgjesse@google.com>
2021-11-01 10:30:57 +0100

    Add android.jar for Android 12L (preview revision 1)
    Content of README.google:
    
    Name: Android system library jar
    URL: https://developer.android.com/about/versions/12/12L
    Version: API version Sv1 (preview revision 1)
    Revision: NA
    License: Apache License Version 2.0
    
    Description:
    This is the android.jar pulled using the android sdk manager.
    
    Bug: 204601181
    Change-Id: Ia5e097247aca82d76de30ac94a4098e89d924cf0

---
+++ scripts/add-android-jar.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright (c) 2021, the R8 project authors. Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+set -e
+set -x
+
+echo "Update this script manually before using"
+exit -1
+
+# Download Platform SDK in @SDK_HOME
+SDK_HOME=$HOME/Android/Sdk
+
+# Modify these to match the SDK android.jar to add.
+SDK_DIR_NAME=android-Sv2
+SDK_VERSION=32
+
+SDK_DIR=$SDK_HOME/platforms/$SDK_DIR_NAME
+THIRD_PARTY_ANDROID_JAR=third_party/android_jar
+THIRD_PARTY_ANDROID_JAR_LIB=$THIRD_PARTY_ANDROID_JAR/lib-v$SDK_VERSION
+
+rm -rf $THIRD_PARTY_ANDROID_JAR_LIB
+rm -f ${THIRD_PARTY_ANDROID_JAR_LIB}.tar.gz
+rm -f ${THIRD_PARTY_ANDROID_JAR_LIB}.tar.sha1
+
+mkdir -p $THIRD_PARTY_ANDROID_JAR_LIB/optional
+cp $SDK_DIR/android.jar $THIRD_PARTY_ANDROID_JAR_LIB/android.jar
+cp $SDK_DIR/optional/*.jar $THIRD_PARTY_ANDROID_JAR_LIB/optional
+cp $SDK_DIR/optional/optional.json $THIRD_PARTY_ANDROID_JAR_LIB/optional
+cp $THIRD_PARTY_ANDROID_JAR/lib-v31/README.google $THIRD_PARTY_ANDROID_JAR_LIB
+vi $THIRD_PARTY_ANDROID_JAR_LIB/README.google
+
+(cd $THIRD_PARTY_ANDROID_JAR \
+	&& upload_to_google_storage.py -a --bucket r8-deps lib-v$SDK_VERSION)
+rm -rf $THIRD_PARTY_ANDROID_JAR_LIB
+rm ${THIRD_PARTY_ANDROID_JAR_LIB}.tar.gz
+git add ${THIRD_PARTY_ANDROID_JAR_LIB}.tar.gz.sha1
+
+echo "Update build.gradle with this new cloud dependency, and verify with tools/gradle.py downloadDeps"
