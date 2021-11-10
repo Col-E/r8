3a701b177c3bc405869a5bb8438208e1e83f9cb4
Clément Béra <clementbera@google.com>
2021-11-10 13:52:04 +0000

    New bazel flag for desugared jdk lib 11
    Change-Id: I57a11c65f7a6612b79f4a09db9d989956aaa2928

--- tools/archive_desugar_jdk_libs.py
+++ tools/archive_desugar_jdk_libs.py
@@ -98,8 +98,9 @@ def BuildDesugaredLibrary(checkout_dir, variant):
         bazel,
         '--bazelrc=/dev/null',
         'build',
-        'maven_release' + ('_jdk11' if variant == 'jdk11' else ''),
-         '--java_language_version=' + ('11' if variant == 'jdk11' else '8')]
+        'maven_release' + ('_jdk11' if variant == 'jdk11' else '')]
+    if variant == 'jdk11':
+        cmd.append('--java_runtime_version=remotejdk_11')
     utils.PrintCmd(cmd)
     subprocess.check_call(cmd)
     cmd = [bazel, 'shutdown']
