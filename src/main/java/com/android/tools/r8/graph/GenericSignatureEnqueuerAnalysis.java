e7afe09c987242719baf707a28fd38bc2c8b9e33
Morten Krogh-Jespersen <mkroghj@google.com>
2021-11-18 13:41:18 +0100

    Visit types in generic signatures for referenced methods and fields
    Bug: 205504423
    Change-Id: I1c7286e44179c58e2c8f88751c3086f378466401

--- src/main/java/com/android/tools/r8/graph/GenericSignatureEnqueuerAnalysis.java
+++ src/main/java/com/android/tools/r8/graph/GenericSignatureEnqueuerAnalysis.java
@@ -7,10 +7,13 @@ package com.android.tools.r8.graph;
 import com.android.tools.r8.graph.analysis.EnqueuerAnalysis;
 import com.android.tools.r8.shaking.Enqueuer.EnqueuerDefinitionSupplier;
 import com.android.tools.r8.shaking.EnqueuerWorklist;
+import com.google.common.collect.Sets;
+import java.util.Set;
 
 public class GenericSignatureEnqueuerAnalysis extends EnqueuerAnalysis {
 
   private final EnqueuerDefinitionSupplier enqueuerDefinitionSupplier;
+  private final Set<DexReference> processedSignatures = Sets.newIdentityHashSet();
 
   public GenericSignatureEnqueuerAnalysis(EnqueuerDefinitionSupplier enqueuerDefinitionSupplier) {
     this.enqueuerDefinitionSupplier = enqueuerDefinitionSupplier;
@@ -18,20 +21,45 @@ public class GenericSignatureEnqueuerAnalysis extends EnqueuerAnalysis {
 
   @Override
   public void processNewlyLiveClass(DexProgramClass clazz, EnqueuerWorklist worklist) {
-    new GenericSignatureTypeVisitor(clazz, enqueuerDefinitionSupplier::definitionFor)
-        .visitClassSignature(clazz.getClassSignature());
+    processSignature(clazz, clazz.getContext());
+  }
+
+  @Override
+  public void notifyMarkFieldAsReachable(ProgramField field) {
+    processSignature(field, field.getContext());
   }
 
   @Override
   public void processNewlyLiveField(ProgramField field, ProgramDefinition context) {
-    new GenericSignatureTypeVisitor(context, enqueuerDefinitionSupplier::definitionFor)
-        .visitFieldTypeSignature(field.getDefinition().getGenericSignature());
+    processSignature(field, context);
+  }
+
+  @Override
+  public void notifyMarkMethodAsTargeted(ProgramMethod method) {
+    processSignature(method, method.getContext());
   }
 
   @Override
   public void processNewlyLiveMethod(ProgramMethod method, ProgramDefinition context) {
-    new GenericSignatureTypeVisitor(context, enqueuerDefinitionSupplier::definitionFor)
-        .visitMethodSignature(method.getDefinition().getGenericSignature());
+    processSignature(method, context);
   }
 
+  private void processSignature(ProgramDefinition signatureHolder, ProgramDefinition context) {
+    if (!processedSignatures.add(signatureHolder.getReference())) {
+      return;
+    }
+    GenericSignatureTypeVisitor genericSignatureTypeVisitor =
+        new GenericSignatureTypeVisitor(context, enqueuerDefinitionSupplier::definitionFor);
+    if (signatureHolder.isClass()) {
+      genericSignatureTypeVisitor.visitClassSignature(
+          signatureHolder.asClass().getClassSignature());
+    } else if (signatureHolder.isMethod()) {
+      genericSignatureTypeVisitor.visitMethodSignature(
+          signatureHolder.asMethod().getDefinition().getGenericSignature());
+    } else {
+      assert signatureHolder.isField();
+      genericSignatureTypeVisitor.visitFieldTypeSignature(
+          signatureHolder.asField().getDefinition().getGenericSignature());
+    }
+  }
 }
