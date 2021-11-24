fb1016307fa4b607dae0ae4f382f40541ec04991
Morten Krogh-Jespersen <mkroghj@google.com>
2021-11-24 13:01:00 +0100

    [ApiModel] Regression test for inlining into method with only typeref
    Bug: 207601948
    Change-Id: If10301420323ef27a9fbdfef7f1c2cbfaf1365f5

---
+++ src/test/java/com/android/tools/r8/apimodel/ApiModelTypeReferenceInvokeTest.java
@@ -0,0 +1,93 @@
+// Copyright (c) 2021, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.apimodel;
+
+import static com.android.tools.r8.apimodel.ApiModelingTestHelper.setMockApiLevelForClass;
+import static com.android.tools.r8.apimodel.ApiModelingTestHelper.setMockApiLevelForMethod;
+import static com.android.tools.r8.apimodel.ApiModelingTestHelper.verifyThat;
+
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.testing.AndroidBuildVersion;
+import com.android.tools.r8.utils.AndroidApiLevel;
+import java.lang.reflect.Method;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ApiModelTypeReferenceInvokeTest extends TestBase {
+
+  @Parameter() public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    Method apiMethod = LibraryClass.class.getDeclaredMethod("apiMethod");
+    Method apiCaller = ApiHelper.class.getDeclaredMethod("apiCaller", LibraryClass.class);
+    Method apiCallerCaller = Main.class.getDeclaredMethod("typeReference", Object.class);
+    boolean libraryClassOnBoot =
+        parameters.isDexRuntime()
+            && parameters.getApiLevel().isGreaterThanOrEqualTo(AndroidApiLevel.M);
+    testForR8(parameters.getBackend())
+        .addProgramClasses(Main.class, ApiHelper.class)
+        .addLibraryClasses(LibraryClass.class)
+        .addDefaultRuntimeLibrary(parameters)
+        .setMinApi(parameters.getApiLevel())
+        .apply(setMockApiLevelForClass(LibraryClass.class, AndroidApiLevel.M))
+        .apply(setMockApiLevelForMethod(apiMethod, AndroidApiLevel.M))
+        .apply(ApiModelingTestHelper::enableApiCallerIdentification)
+        .addKeepMainRule(Main.class)
+        .enableInliningAnnotations()
+        .addAndroidBuildVersion()
+        .compile()
+        // TODO(b/207601948): We should only inline from api level M.
+        .inspect(verifyThat(parameters, apiCaller).inlinedInto(apiCallerCaller))
+        .addRunClasspathClasses(LibraryClass.class)
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLinesIf(
+            libraryClassOnBoot, "LibraryClass::apiMethod", "Hello World")
+        .assertSuccessWithOutputLinesIf(!libraryClassOnBoot, "Hello World");
+  }
+
+  public static class LibraryClass {
+
+    public void apiMethod() {
+      System.out.println("LibraryClass::apiMethod");
+    }
+  }
+
+  public static class ApiHelper {
+
+    public static void apiCaller(LibraryClass libraryClass) {
+      if (AndroidBuildVersion.VERSION >= 23) {
+        libraryClass.apiMethod();
+      }
+    }
+  }
+
+  public static class Main {
+
+    @NeverInline
+    public static void typeReference(Object object) {
+      if (object instanceof LibraryClass) {
+        ApiHelper.apiCaller((LibraryClass) object);
+      }
+    }
+
+    public static void main(String[] args) {
+      typeReference(args.length == 0 ? new LibraryClass() : "Foo");
+      System.out.println("Hello World");
+    }
+  }
+}
