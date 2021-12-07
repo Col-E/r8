d6e5c3c45019fb52143ec8d5e32e3e89a646c20e
Christoffer Quist Adamsen <christofferqa@google.com>
2021-12-07 15:52:56 +0100

    Account for feature splits in field type strengthening
    Bug: 209469840
    Change-Id: I37e693147c1a730dc73114d9a47f7a5caf87de9c

--- src/main/java/com/android/tools/r8/utils/AccessUtils.java
+++ src/main/java/com/android/tools/r8/utils/AccessUtils.java
@@ -4,33 +4,56 @@
 
 package com.android.tools.r8.utils;
 
+import com.android.tools.r8.FeatureSplit;
+import com.android.tools.r8.features.ClassToFeatureSplitMap;
+import com.android.tools.r8.graph.AppView;
 import com.android.tools.r8.graph.DexClass;
-import com.android.tools.r8.graph.DexDefinitionSupplier;
 import com.android.tools.r8.graph.DexItemFactory;
 import com.android.tools.r8.graph.DexType;
+import com.android.tools.r8.shaking.AppInfoWithLiveness;
+import com.android.tools.r8.synthesis.SyntheticItems;
 
 public class AccessUtils {
 
   public static boolean isAccessibleInSameContextsAs(
-      DexType newType, DexType oldType, DexDefinitionSupplier definitions) {
-    DexItemFactory dexItemFactory = definitions.dexItemFactory();
+      DexType newType, DexType oldType, AppView<AppInfoWithLiveness> appView) {
+    DexItemFactory dexItemFactory = appView.dexItemFactory();
     DexType newBaseType = newType.toBaseType(dexItemFactory);
     if (!newBaseType.isClassType()) {
       return true;
     }
-    DexClass newBaseClass = definitions.definitionFor(newBaseType);
+    DexClass newBaseClass = appView.definitionFor(newBaseType);
     if (newBaseClass == null) {
       return false;
     }
-    if (newBaseClass.isPublic()) {
-      return true;
-    }
+
+    // If the new class is not public, then the old class must be non-public as well and reside in
+    // the same package as the new class.
     DexType oldBaseType = oldType.toBaseType(dexItemFactory);
-    assert oldBaseType.isClassType();
-    DexClass oldBaseClass = definitions.definitionFor(oldBaseType);
-    if (oldBaseClass == null || oldBaseClass.isPublic()) {
-      return false;
+    if (!newBaseClass.isPublic()) {
+      assert oldBaseType.isClassType();
+      DexClass oldBaseClass = appView.definitionFor(oldBaseType);
+      if (oldBaseClass == null
+          || oldBaseClass.isPublic()
+          || !newBaseType.isSamePackage(oldBaseType)) {
+        return false;
+      }
     }
-    return newBaseType.isSamePackage(oldBaseType);
+
+    // If the new class is a program class, we need to check if it is in a feature.
+    if (newBaseClass.isProgramClass()) {
+      ClassToFeatureSplitMap classToFeatureSplitMap = appView.appInfo().getClassToFeatureSplitMap();
+      SyntheticItems syntheticItems = appView.getSyntheticItems();
+      if (classToFeatureSplitMap != null) {
+        FeatureSplit newFeatureSplit =
+            classToFeatureSplitMap.getFeatureSplit(newBaseClass.asProgramClass(), syntheticItems);
+        if (!newFeatureSplit.isBase()
+            && newFeatureSplit
+                != classToFeatureSplitMap.getFeatureSplit(oldBaseType, syntheticItems)) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
---
+++ src/test/java/com/android/tools/r8/dexsplitter/DexSplitterFieldTypeStrengtheningTest.java
@@ -0,0 +1,122 @@
+// Copyright (c) 2021, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.dexsplitter;
+
+import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+
+import com.android.tools.r8.CompilationFailedException;
+import com.android.tools.r8.R8TestCompileResult;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.TestShrinkerBuilder;
+import com.android.tools.r8.ThrowableConsumer;
+import com.android.tools.r8.ToolHelper.ProcessResult;
+import com.android.tools.r8.utils.StringUtils;
+import com.android.tools.r8.utils.ThrowingConsumer;
+import com.android.tools.r8.utils.codeinspector.ClassSubject;
+import com.android.tools.r8.utils.codeinspector.FieldSubject;
+import com.google.common.collect.ImmutableSet;
+import java.io.IOException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class DexSplitterFieldTypeStrengtheningTest extends SplitterTestBase {
+
+  public static final String EXPECTED = StringUtils.lines("FeatureClass");
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection params() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  private final TestParameters parameters;
+
+  public DexSplitterFieldTypeStrengtheningTest(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void testPropagationFromFeature() throws Exception {
+    ThrowingConsumer<R8TestCompileResult, Exception> ensureGetFromFeatureGone =
+        r8TestCompileResult -> {
+          // Ensure that getFromFeature from FeatureClass is inlined into the run method.
+          ClassSubject clazz = r8TestCompileResult.inspector().clazz(FeatureClass.class);
+          assertThat(clazz.uniqueMethodWithName("getFromFeature"), not(isPresent()));
+        };
+    ProcessResult processResult =
+        testDexSplitter(
+            parameters,
+            ImmutableSet.of(BaseSuperClass.class),
+            ImmutableSet.of(FeatureClass.class),
+            FeatureClass.class,
+            EXPECTED,
+            ensureGetFromFeatureGone,
+            TestShrinkerBuilder::noMinification);
+    // We expect art to fail on this with the dex splitter, see b/122902374
+    assertNotEquals(processResult.exitCode, 0);
+    assertTrue(processResult.stderr.contains("NoClassDefFoundError"));
+  }
+
+  @Test
+  public void testOnR8Splitter() throws IOException, CompilationFailedException {
+    assumeTrue(parameters.isDexRuntime());
+    ProcessResult processResult =
+        testR8Splitter(
+            parameters,
+            ImmutableSet.of(BaseSuperClass.class),
+            ImmutableSet.of(FeatureClass.class),
+            FeatureClass.class,
+            compileResult ->
+                compileResult.inspect(
+                    inspector -> {
+                      ClassSubject baseSuperClassSubject = inspector.clazz(BaseSuperClass.class);
+                      assertThat(baseSuperClassSubject, isPresent());
+
+                      FieldSubject fieldSubject = baseSuperClassSubject.uniqueFieldWithName("f");
+                      assertThat(fieldSubject, isPresent());
+                      assertEquals(
+                          Object.class.getTypeName(),
+                          fieldSubject.getField().getType().getTypeName());
+                    }),
+            ThrowableConsumer.empty());
+    assertEquals(processResult.exitCode, 0);
+    assertEquals(processResult.stdout, EXPECTED);
+  }
+
+  public abstract static class BaseSuperClass implements RunInterface {
+
+    public static Object f;
+
+    @Override
+    public void run() {
+      setFieldFromFeature();
+      System.out.println(f);
+    }
+
+    public abstract void setFieldFromFeature();
+  }
+
+  public static class FeatureClass extends BaseSuperClass {
+
+    @Override
+    public void setFieldFromFeature() {
+      BaseSuperClass.f = this;
+    }
+
+    @Override
+    public String toString() {
+      return "FeatureClass";
+    }
+  }
+}
