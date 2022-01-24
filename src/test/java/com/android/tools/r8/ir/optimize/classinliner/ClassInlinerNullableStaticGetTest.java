4ee5d3f8fbd3919ea679e1c9e22c2e86f1b4cce5
Christoffer Quist Adamsen <christofferqa@google.com>
2022-01-24 07:32:34 +0100

    Reproduce class inlining of nullable field
    Bug: 215742573
    Change-Id: I0ece7426777d6d1590448c08bf89e72f0d4f691d

---
+++ src/test/java/com/android/tools/r8/ir/optimize/classinliner/ClassInlinerNullableStaticGetTest.java
@@ -0,0 +1,71 @@
+// Copyright (c) 2022, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.ir.optimize.classinliner;
+
+import com.android.tools.r8.NoVerticalClassMerging;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ClassInlinerNullableStaticGetTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .addKeepMainRule(Main.class)
+        .enableNoVerticalClassMergingAnnotations()
+        .setMinApi(parameters.getApiLevel())
+        .compile()
+        .run(parameters.getRuntime(), Main.class)
+        // TODO(b/215742573): Should fail with NPE.
+        .assertSuccessWithOutputLines("Hello!");
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      Singleton.get().greet();
+      Singleton.set();
+      if (System.currentTimeMillis() < 0) {
+        System.out.println(new Singleton());
+      }
+    }
+  }
+
+  @NoVerticalClassMerging
+  static class Singleton {
+
+    static Singleton INSTANCE;
+
+    static Singleton get() {
+      return INSTANCE;
+    }
+
+    static void set() {
+      INSTANCE = new SingletonImpl();
+    }
+
+    void greet() {
+      System.out.println("Hello!");
+    }
+  }
+
+  static class SingletonImpl extends Singleton {}
+}
