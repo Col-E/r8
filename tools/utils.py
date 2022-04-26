e83c378efcc0fda820bed2540854ae3a6bfa8af7
Rico Wind <ricow@google.com>
2022-04-26 12:38:15 +0200

    Never set public-read on gcs uploads
    I will remove all callsites in a follow up cl to make this easy to merge
    
    Bug: 177799191
    Change-Id: I6b4a51099a441e95e58b5708af5738fc21f8cc8f

--- tools/utils.py
+++ tools/utils.py
@@ -343,18 +343,12 @@ def upload_dir_to_cloud_storage(directory, destination, is_html=False, public_re
   cmd = [get_gsutil(), '-m', 'cp']
   if is_html:
     cmd += ['-z', 'html']
-  if public_read:
-    # TODO(b/177799191) Temporarily disable public-read to test uniform access control
-    if 'r8-test-results' not in destination:
-      cmd += ['-a', 'public-read']
   cmd += ['-R', directory, destination]
   PrintCmd(cmd)
   subprocess.check_call(cmd)
 
 def upload_file_to_cloud_storage(source, destination, public_read=True):
   cmd = [get_gsutil(), 'cp']
-  if public_read:
-    cmd += ['-a', 'public-read']
   cmd += [source, destination]
   PrintCmd(cmd)
   subprocess.check_call(cmd)
