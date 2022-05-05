1f56b7a18cdf8a2928b8e767ec025f5cd0ede281
Morten Krogh-Jespersen <mkroghj@google.com>
2022-05-05 14:19:09 +0200

    Add clarifying paranthesis to ByteUtils
    Change-Id: Ie1b55806d33b04f35d0060d5990f23feadb80c6f

--- src/main/java/com/android/tools/r8/lightir/ByteUtils.java
+++ src/main/java/com/android/tools/r8/lightir/ByteUtils.java
@@ -7,7 +7,7 @@ package com.android.tools.r8.lightir;
 public class ByteUtils {
 
   public static boolean isU1(int value) {
-    return 0 <= value && value <= 0xFF;
+    return (0 <= value) && (value <= 0xFF);
   }
 
   // Lossy truncation of an integer value to its lowest byte.
@@ -37,14 +37,14 @@ public class ByteUtils {
   }
 
   public static boolean isU2(int value) {
-    return value >= 0 && value <= 0xFFFF;
+    return (value >= 0) && (value <= 0xFFFF);
   }
 
   public static int unsetBitAtIndex(int value, int index) {
-    return value & ~(1 << index - 1);
+    return value & ~(1 << (index - 1));
   }
 
   public static int setBitAtIndex(int value, int index) {
-    return value | (1 << index - 1);
+    return value | (1 << (index - 1));
   }
 }
