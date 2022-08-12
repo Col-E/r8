37177e9dd50dc6adf50c65e6b3e36beee1c2689a
Rico Wind <ricow@google.com>
2022-08-12 06:37:56 +0200

    Add regression test for b/241478253
    In debug mode we fail to see the reflective usage of the constructor
    
    Change-Id: I83820c274caa7b9c28074c89ec4b22af0f91e4c6

---
+++ src/test/java/com/android/tools/r8/regress/Regress241478253.java
@@ -0,0 +1,67 @@
+// Copyright (c) 2022, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.regress;
+
+import com.android.tools.r8.R8TestRunResult;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class Regress241478253 extends TestBase {
+
+  static final String EXPECTED = "In the Bar";
+
+  private final TestParameters parameters;
+
+  @Parameterized.Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withDexRuntimes().withAllApiLevels().build();
+  }
+
+  public Regress241478253(TestParameters parameters) {
+    this.parameters = parameters;
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    R8TestRunResult r8Release =
+        testForR8(parameters.getBackend())
+            .addProgramClasses(Foo.class, Bar.class)
+            .addKeepMainRule(Foo.class)
+            .setMinApi(parameters.getApiLevel())
+            .run(parameters.getRuntime(), Foo.class)
+            .assertSuccessWithOutputLines(EXPECTED);
+    R8TestRunResult r8Debug =
+        testForR8(parameters.getBackend())
+            .debug()
+            .addProgramClasses(Foo.class, Bar.class)
+            .addKeepMainRule(Foo.class)
+            .setMinApi(parameters.getApiLevel())
+            .run(parameters.getRuntime(), Foo.class)
+            // TODO(b/241478253): Should succeed
+            .assertFailureWithErrorThatThrows(NoSuchMethodException.class);
+  }
+
+  public static class Foo {
+    public static void main(String[] args) {
+      try {
+        Class<Bar> impl =
+            (Class<Bar>) Class.forName("com.android.tools.r8.regress.Regress241478253$Bar");
+        impl.getDeclaredConstructor().newInstance();
+      } catch (Exception ignored) {
+        throw new RuntimeException(ignored);
+      }
+    }
+  }
+
+  public static class Bar {
+    public Bar() {
+      System.out.println("In the Bar");
+    }
+  }
+}
