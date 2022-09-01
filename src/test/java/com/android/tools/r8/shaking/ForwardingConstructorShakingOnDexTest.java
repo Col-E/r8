5f3a6dbbb07a7e950181f6d216a5c822e0782955
Christoffer Quist Adamsen <christofferqa@google.com>
2022-09-01 08:43:06 +0200

    Tests for constructor shrinking
    Bug: b/173751869
    Change-Id: Ia1dcea3b6ac2e033570f3f289a2244aa56076f36

---
+++ src/test/java/com/android/tools/r8/shaking/ForwardingConstructorShakingOnDexTest.java
@@ -0,0 +1,124 @@
+// Copyright (c) 2022, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.shaking;
+
+import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import com.android.tools.r8.KeepConstantArguments;
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.NoVerticalClassMerging;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.codeinspector.ClassSubject;
+import com.android.tools.r8.utils.codeinspector.CodeInspector;
+import com.android.tools.r8.utils.codeinspector.FoundMethodSubject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ForwardingConstructorShakingOnDexTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .addKeepMainRule(Main.class)
+        .enableConstantArgumentAnnotations()
+        .enableNeverClassInliningAnnotations()
+        .enableNoVerticalClassMergingAnnotations()
+        .setMinApi(parameters.getApiLevel())
+        .compile()
+        .inspect(this::inspect)
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("A", "A(String)", "B", "B(String)", "C", "C(String)");
+  }
+
+  private void inspect(CodeInspector inspector) {
+    ClassSubject aClassSubject = inspector.clazz(A.class);
+    assertThat(aClassSubject, isPresent());
+    assertEquals(2, aClassSubject.allMethods(FoundMethodSubject::isInstanceInitializer).size());
+
+    ClassSubject bClassSubject = inspector.clazz(B.class);
+    assertThat(bClassSubject, isPresent());
+    // TODO(b/173751869): Should be 0 when compiling for dex and API is above Dalvik.
+    assertEquals(2, bClassSubject.allMethods(FoundMethodSubject::isInstanceInitializer).size());
+
+    ClassSubject cClassSubject = inspector.clazz(C.class);
+    assertThat(cClassSubject, isPresent());
+    // TODO(b/173751869): Should be 0 when compiling for dex and API is above Dalvik.
+    assertEquals(2, cClassSubject.allMethods(FoundMethodSubject::isInstanceInitializer).size());
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      new A();
+      new A("A(String)");
+      new B();
+      new B("B(String)");
+      new C();
+      new C("C(String)");
+    }
+  }
+
+  @NeverClassInline
+  @NoVerticalClassMerging
+  static class A {
+
+    A() {
+      if (this instanceof C) {
+        System.out.println("C");
+      } else if (this instanceof B) {
+        System.out.println("B");
+      } else {
+        System.out.println("A");
+      }
+    }
+
+    A(String string) {
+      System.out.println(string);
+    }
+  }
+
+  @NeverClassInline
+  @NoVerticalClassMerging
+  static class B extends A {
+
+    // These constructors simply forward the arguments to the parent constructor.
+    // They can be removed when compiling for dex and the API is above Dalvik.
+    B() {}
+
+    B(String string) {
+      super(string);
+    }
+  }
+
+  @NeverClassInline
+  @NoVerticalClassMerging
+  static class C extends B {
+
+    // Ditto.
+    C() {}
+
+    @KeepConstantArguments
+    C(String string) {
+      super(string);
+    }
+  }
+}
---
+++ src/test/java/com/android/tools/r8/shaking/RetainIndirectlyReferencedConstructorShakingOnDexTest.java
@@ -0,0 +1,93 @@
+// Copyright (c) 2022, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.shaking;
+
+import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import com.android.tools.r8.NoVerticalClassMerging;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.codeinspector.ClassSubject;
+import com.android.tools.r8.utils.codeinspector.CodeInspector;
+import com.android.tools.r8.utils.codeinspector.FoundMethodSubject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class RetainIndirectlyReferencedConstructorShakingOnDexTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .addKeepMainRule(Main.class)
+        .enableNoVerticalClassMergingAnnotations()
+        .setMinApi(parameters.getApiLevel())
+        .compile()
+        .inspect(this::inspect)
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("A", "B");
+  }
+
+  private void inspect(CodeInspector inspector) {
+    // A.<init> should be retained despite the fact that there is no invoke-direct in the program
+    // that directly targets A.<init> when B.<init> is removed.
+    ClassSubject aClassSubject = inspector.clazz(A.class);
+    assertThat(aClassSubject, isPresent());
+    assertEquals(1, aClassSubject.allMethods(FoundMethodSubject::isInstanceInitializer).size());
+
+    ClassSubject bClassSubject = inspector.clazz(B.class);
+    assertThat(bClassSubject, isPresent());
+    // TODO(b/173751869): Should be 0 when compiling for dex and API is above Dalvik.
+    assertEquals(1, bClassSubject.allMethods(FoundMethodSubject::isInstanceInitializer).size());
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      // This uses an invoke-direct instruction that targets B.<init>(). If we remove B.<init>()
+      // when compiling for dex, and keep the invoke-direct instruction targeting B.<init>(), it is
+      // important that tree shaking traces A.<init>(), by resolving the invoke in the Enqueuer,
+      // instead of trying to lookup <init>() directly on B. Otherwise, the program will fail with
+      // NSME.
+      System.out.println(new B());
+    }
+  }
+
+  @NoVerticalClassMerging
+  abstract static class A {
+
+    A() {
+      System.out.println("A");
+    }
+  }
+
+  @NoVerticalClassMerging
+  static class B extends A {
+
+    // This constructor simply forward the arguments to the parent constructor.
+    // It can be removed when compiling for dex and the API is above Dalvik.
+    B() {}
+
+    @Override
+    public String toString() {
+      return "B";
+    }
+  }
+}
