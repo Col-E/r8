3bdde69172f82f0afb41b44aba49b870cb7075cd
Morten Krogh-Jespersen <mkroghj@google.com>
2022-10-26 14:33:01 +0200

    [Compose] Ignore compose composition test until fixed
    Bug: b/255292908
    Change-Id: Ie85785050247642a538a9583679c81e3f30ade0f

--- src/test/java/com/android/tools/r8/retrace/RetraceStackTraceFunctionalCompositionTest.java
+++ src/test/java/com/android/tools/r8/retrace/RetraceStackTraceFunctionalCompositionTest.java
@@ -36,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Supplier;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -156,6 +157,7 @@ public class RetraceStackTraceFunctionalCompositionTest extends TestBase {
     return rewrittenR8Jar;
   }
 
+  @Ignore("b/255292908")
   @Test
   public void testR8RetraceAndComposition() throws Exception {
     Path rewrittenR8Jar = getRewrittenR8Jar();
