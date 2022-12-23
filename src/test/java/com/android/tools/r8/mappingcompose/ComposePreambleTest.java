5e9338d933b071ed28607918c0bc32364d39f683
Morten Krogh-Jespersen <mkroghj@google.com>
2022-12-23 15:07:36 +0100

    [Compose] Add test for not correctly composing if inserted preamble
    Bug: b/241763080
    Change-Id: If9e4924634f3d13d8ff17c6f4458aaff344213d1

---
+++ src/test/java/com/android/tools/r8/mappingcompose/ComposePreambleTest.java
@@ -0,0 +1,60 @@
+// Copyright (c) 2022, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.mappingcompose;
+
+import static com.android.tools.r8.mappingcompose.ComposeTestHelpers.doubleToSingleQuote;
+import static org.junit.Assert.assertEquals;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.naming.ClassNameMapper;
+import com.android.tools.r8.naming.MappingComposer;
+import com.android.tools.r8.utils.StringUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ComposePreambleTest extends TestBase {
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withNoneRuntime().build();
+  }
+
+  public ComposePreambleTest(TestParameters parameters) {
+    parameters.assertNoneRuntime();
+  }
+
+  private static final String mappingFoo =
+      StringUtils.unixLines(
+          "# {'id':'com.android.tools.r8.mapping','version':'2.2'}",
+          "com.foo -> a:",
+          "    1:2:int f1():41:42 -> g1",
+          "    112:112:void f2():112:112 -> g2");
+  private static final String mappingBar =
+      StringUtils.unixLines(
+          "# {'id':'com.android.tools.r8.mapping','version':'2.2'}",
+          "a -> b:",
+          "    1:3:int g1():0:2 -> h1",
+          "    0:65535:void g2():112:112 -> h2");
+  private static final String mappingResult =
+      // TODO(b/241763080): Should map g1 correctly even if it has a preamble line.
+      StringUtils.unixLines(
+          "# {'id':'com.android.tools.r8.mapping','version':'2.2'}",
+          "com.foo -> b:",
+          "    1:3:int g1():0:2 -> h1",
+          "    0:65535:void f2():112:112 -> h2");
+
+  @Test
+  public void testCompose() throws Exception {
+    ClassNameMapper mappingForFoo = ClassNameMapper.mapperFromString(mappingFoo);
+    ClassNameMapper mappingForBar = ClassNameMapper.mapperFromString(mappingBar);
+    String composed = MappingComposer.compose(mappingForFoo, mappingForBar);
+    assertEquals(mappingResult, doubleToSingleQuote(composed));
+  }
+}
