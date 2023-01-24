57ee0e61a093b506aec5e8590f8dda42e80ed081
Morten Krogh-Jespersen <mkroghj@google.com>
2023-01-24 12:27:40 +0100

    [Compose] Enable map composition by default
    Bug: b/241763080
    Change-Id: Ie23989e44d4c581651013e06350320a81f9c14ad

--- src/main/java/com/android/tools/r8/utils/InternalOptions.java
+++ src/main/java/com/android/tools/r8/utils/InternalOptions.java
@@ -1800,8 +1800,9 @@ public class InternalOptions implements GlobalKeepInfoConfiguration {
   }
 
   public static class MappingComposeOptions {
-    // TODO(b/241763080): Remove when enabled.
-    public boolean enableExperimentalMappingComposition = false;
+    public boolean enableExperimentalMappingComposition =
+        !parseSystemPropertyForDevelopmentOrDefault(
+            "com.android.tools.r8.disableMapComposition", false);
     // TODO(b/247136434): Disable for internal builds.
     public boolean allowNonExistingOriginalRanges = true;
     public Consumer<ClassNameMapper> generatedClassNameMapperConsumer = null;
