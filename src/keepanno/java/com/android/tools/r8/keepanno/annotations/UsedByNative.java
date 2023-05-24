89bc5359bb98d0df12a85be05a215e60a8ef5419
Ian Zerny <zerny@google.com>
2023-05-24 12:54:39 +0200

    [KeepAnno] Add annotations UsedByReflection and UsedByNative
    Bug: b/248408342
    Bug: b/284104251
    Change-Id: If29d482c252805ce69d6591036e9115bbcde711b

---
+++ src/keepanno/java/com/android/tools/r8/keepanno/annotations/UsedByNative.java
@@ -0,0 +1,66 @@
+// Copyright (c) 2023, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.keepanno.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to mark a class, field or method as being accessed from native code via JNI.
+ *
+ * <p>When a class is annotated, member patterns can be used to define which members are to be kept.
+ * When no member patterns are specified the default pattern is to match just the class.
+ *
+ * <p>When a member is annotated, the member patterns cannot be used as the annotated member itself
+ * fully defines the item to be kept (i.e., itself).
+ */
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Retention(RetentionPolicy.CLASS)
+public @interface UsedByNative {
+  String description() default "";
+
+  /**
+   * Conditions that should be satisfied for the annotation to be in effect.
+   *
+   * <p>Defaults to no conditions, thus trivially/unconditionally satisfied.
+   */
+  KeepCondition[] preconditions() default {};
+
+  /** Additional targets to be kept in addition to the annotated class/members. */
+  KeepTarget[] additionalTargets() default {};
+
+  /**
+   * The target kind to be kept.
+   *
+   * <p>When annotating a class without member patterns, the default kind is {@link
+   * KeepItemKind#ONLY_CLASS}.
+   *
+   * <p>When annotating a class with member patterns, the default kind is {@link
+   * KeepItemKind#CLASS_AND_MEMBERS}.
+   *
+   * <p>When annotating a member, the default kind is {@link KeepItemKind#ONLY_MEMBERS}.
+   *
+   * <p>It is not possible to use ONLY_CLASS if annotating a member.
+   */
+  KeepItemKind kind() default KeepItemKind.DEFAULT;
+
+  // Member patterns. See KeepTarget for documentation.
+  MemberAccessFlags[] memberAccess() default {};
+
+  MethodAccessFlags[] methodAccess() default {};
+
+  String methodName() default "";
+
+  String methodReturnType() default "";
+
+  String[] methodParameters() default {""};
+
+  FieldAccessFlags[] fieldAccess() default {};
+
+  String fieldName() default "";
+
+  String fieldType() default "";
+}
