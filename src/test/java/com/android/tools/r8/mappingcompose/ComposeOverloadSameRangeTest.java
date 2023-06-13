47da9f7c4d681188257c8620c423ed27978a4a09
Morten Krogh-Jespersen <mkroghj@google.com>
2023-06-13 17:14:51 +0200

    Add test for composing overloaded methods with same range
    Bug: b/286781273
    Change-Id: Id6f43f4c29ffdebbde6722ef560fd55e8b07fd79

---
+++ src/test/java/com/android/tools/r8/mappingcompose/ComposeOverloadSameRangeTest.java
@@ -0,0 +1,57 @@
+// Copyright (c) 2023, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.android.tools.r8.mappingcompose;
+
+import static org.junit.Assert.assertThrows;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.naming.ClassNameMapper;
+import com.android.tools.r8.naming.MappingComposer;
+import com.android.tools.r8.utils.StringUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/** This is a regression test for b/286781273. */
+@RunWith(Parameterized.class)
+public class ComposeOverloadSameRangeTest extends TestBase {
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withNoneRuntime().build();
+  }
+
+  public ComposeOverloadSameRangeTest(TestParameters parameters) {
+    parameters.assertNoneRuntime();
+  }
+
+  private static final String mappingFoo =
+      StringUtils.unixLines(
+          "# {'id':'com.android.tools.r8.mapping','version':'2.2'}",
+          "com.foo -> a:",
+          "    1:2:void method():41:42 -> m",
+          "    3:3:void inlinee():20:20 -> m",
+          "    3:3:void method():43 -> m",
+          "    1:2:void method(int):111:112 -> m",
+          "    3:3:void method(int):113:113 -> m");
+  private static final String mappingBar =
+      StringUtils.unixLines(
+          "# {'id':'com.android.tools.r8.mapping','version':'2.2'}",
+          "a -> b:",
+          "    1:3:void m():1:3 -> m",
+          "    1:3:void m(int):1:3 -> m");
+  private static final String mappingResult = StringUtils.unixLines();
+
+  @Test
+  public void testCompose() throws Exception {
+    ClassNameMapper mappingForFoo = ClassNameMapper.mapperFromStringWithPreamble(mappingFoo);
+    ClassNameMapper mappingForBar = ClassNameMapper.mapperFromStringWithPreamble(mappingBar);
+    // TODO(b/286781273): Handle same range for overloaded methods.
+    assertThrows(AssertionError.class, () -> MappingComposer.compose(mappingForFoo, mappingForBar));
+  }
+}
