dfc36890a4acca6d4ff4b03152c8b138c21224ab
Ian Zerny <zerny@google.com>
2023-08-25 14:11:57 +0200

    Add option to use compiledump in the new gradle build
    Change-Id: I6ccd6589291bbe8b9e273fa5d9f012c47b51d5a5

--- tools/compiledump.py
+++ tools/compiledump.py
@@ -149,6 +149,11 @@ def make_parser():
     help='Run compilation in specified mode',
     choices=['debug', 'release'],
     default=None)
+  parser.add_argument(
+    '--new-gradle',
+    help='Use the new gradle build (defaults to old build)',
+    default=False,
+    action='store_true')
   return parser
 
 def error(msg):
@@ -378,9 +383,15 @@ def determine_properties(build_properties):
         args.append('-D' + name + '=' + value)
   return args
 
-def download_distribution(version, nolib, temp):
+def download_distribution(version, args, temp):
+  nolib = args.nolib
   if version == 'main':
-    return utils.R8_JAR if nolib else utils.R8LIB_JAR
+    if args.new_gradle:
+      return "%s:%s" % (
+        "d8_r8/main/build/libs/deps.jar",
+        "d8_r8/main/build/libs/r8.jar")
+    else:
+      return utils.R8_JAR if nolib else utils.R8LIB_JAR
   if version == 'source':
     return '%s:%s' % (utils.BUILD_JAVA_MAIN_DIR, utils.ALL_DEPS_JAR)
   name = 'r8.jar' if nolib else 'r8lib.jar'
@@ -492,7 +503,7 @@ def run1(out, args, otherargs, jdkhome=None, worker_id=None):
     android_platform_build = determine_android_platform_build(args, build_properties)
     enable_missing_library_api_modeling = determine_enable_missing_library_api_modeling(args, build_properties)
     mode = determine_compilation_mode(args, build_properties)
-    jar = args.r8_jar if args.r8_jar else download_distribution(version, args.nolib, temp)
+    jar = args.r8_jar if args.r8_jar else download_distribution(version, args, temp)
     if ':' not in jar and not os.path.exists(jar):
       error("Distribution does not exist: " + jar)
     cmd = [jdk.GetJavaExecutable(jdkhome)]
