8dec999686b7e4428fd8b1a414fd2ea524e13225
Clément Béra <clementbera@google.com>
2023-08-30 13:56:28 +0200

    Update desugared jdk lib
    Change-Id: Id693b61a5ad075ab43a982dd773d739623c58d1c

--- src/test/java/com/android/tools/r8/desugar/desugaredlibrary/MonthTest.java
+++ src/test/java/com/android/tools/r8/desugar/desugaredlibrary/MonthTest.java
@@ -5,10 +5,12 @@
 package com.android.tools.r8.desugar.desugaredlibrary;
 
 import static com.android.tools.r8.desugar.desugaredlibrary.test.CompilationSpecification.DEFAULT_SPECIFICATIONS;
+import static com.android.tools.r8.desugar.desugaredlibrary.test.LibraryDesugaringSpecification.JDK8;
 import static com.android.tools.r8.desugar.desugaredlibrary.test.LibraryDesugaringSpecification.getJdk8Jdk11;
 
 import com.android.tools.r8.TestParameters;
-import com.android.tools.r8.TestRuntime;
+import com.android.tools.r8.TestRuntime.CfVm;
+import com.android.tools.r8.ToolHelper.DexVm.Version;
 import com.android.tools.r8.desugar.desugaredlibrary.test.CompilationSpecification;
 import com.android.tools.r8.desugar.desugaredlibrary.test.LibraryDesugaringSpecification;
 import com.android.tools.r8.utils.StringUtils;
@@ -24,8 +26,22 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class MonthTest extends DesugaredLibraryTestBase {
 
-  private static final String EXPECTED_JAVA_8_OUTPUT = StringUtils.lines("4");
-  private static final String EXPECTED_JAVA_9_OUTPUT = StringUtils.lines("April");
+  private static final String MISSING_STANDALONE = StringUtils.lines("4", "4", "4");
+  private static final String UK_EXPECTED_RESULT = StringUtils.lines("April", "Apr", "A");
+
+  private static final String RU_EXPECTED_RESULT_DOT = StringUtils.lines("апреля", "апр.", "А");
+  private static final String RU_EXPECTED_RESULT = StringUtils.lines("апреля", "апр", "А");
+  private static final String RU_EXPECTED_RESULT_DOT_NARROW_LOWERCASE =
+      StringUtils.lines("апреля", "апр.", "а");
+
+  private static final String RU_STANDALONE_EXPECTED_RESULT_ALL_UPPERCASE =
+      StringUtils.lines("Апрель", "Апр.", "А");
+  private static final String RU_STANDALONE_EXPECTED_RESULT_ALL_LOWERCASE =
+      StringUtils.lines("апрель", "апр.", "а");
+  private static final String RU_STANDALONE_EXPECTED_RESULT_NARROW_UPPERCASE =
+      StringUtils.lines("апрель", "апр.", "А");
+  private static final String RU_STANDALONE_EXPECTED_RESULT_LONG_NARROW_UPPERCASE =
+      StringUtils.lines("Апрель", "апр.", "А");
 
   private final TestParameters parameters;
   private final LibraryDesugaringSpecification libraryDesugaringSpecification;
@@ -48,18 +64,59 @@ public class MonthTest extends DesugaredLibraryTestBase {
     this.compilationSpecification = compilationSpecification;
   }
 
-  private String getExpectedResult(TestParameters parameters) {
+  private String getUKStandaloneExpectedResult() {
+    if (parameters.isCfRuntime() && parameters.getRuntime().asCf().isOlderThan(CfVm.JDK9)) {
+      return MISSING_STANDALONE;
+    }
+    if (parameters.isDexRuntime()
+        && libraryDesugaringSpecification.hasTimeDesugaring(parameters)
+        && libraryDesugaringSpecification == JDK8) {
+      return MISSING_STANDALONE;
+    }
+    return UK_EXPECTED_RESULT;
+  }
+
+  private String getRUExpectedResult() {
     if (parameters.isCfRuntime()) {
-      if (parameters.getRuntime().asCf().isNewerThan(TestRuntime.CfVm.JDK8)) {
-        return EXPECTED_JAVA_9_OUTPUT;
+      if (parameters.getRuntime().asCf().isOlderThan(CfVm.JDK9)) {
+        return RU_EXPECTED_RESULT;
       }
-      return EXPECTED_JAVA_8_OUTPUT;
+      return RU_EXPECTED_RESULT_DOT;
     }
-    assert parameters.isDexRuntime();
     if (libraryDesugaringSpecification.hasTimeDesugaring(parameters)) {
-      return EXPECTED_JAVA_8_OUTPUT;
+      return RU_EXPECTED_RESULT_DOT_NARROW_LOWERCASE;
     }
-    return EXPECTED_JAVA_9_OUTPUT;
+    return RU_EXPECTED_RESULT_DOT;
+  }
+
+  private String getRUStandaloneExpectedResult() {
+    if (parameters.isCfRuntime()) {
+      if (parameters.getRuntime().asCf().isOlderThan(CfVm.JDK9)) {
+        return RU_STANDALONE_EXPECTED_RESULT_ALL_UPPERCASE;
+      }
+      return RU_STANDALONE_EXPECTED_RESULT_NARROW_UPPERCASE;
+    }
+    if (libraryDesugaringSpecification.hasTimeDesugaring(parameters)) {
+      if (libraryDesugaringSpecification == JDK8) {
+        return MISSING_STANDALONE;
+      }
+      Version version = parameters.getDexRuntimeVersion();
+      if (version.isOlderThan(Version.V4_4_4)) {
+        return RU_STANDALONE_EXPECTED_RESULT_LONG_NARROW_UPPERCASE;
+      }
+      if (version.isOlderThan(Version.V6_0_1)) {
+        return RU_STANDALONE_EXPECTED_RESULT_ALL_UPPERCASE;
+      }
+      return RU_STANDALONE_EXPECTED_RESULT_ALL_LOWERCASE;
+    }
+    return RU_STANDALONE_EXPECTED_RESULT_NARROW_UPPERCASE;
+  }
+
+  private String getExpectedResult() {
+    return getUKStandaloneExpectedResult()
+        + UK_EXPECTED_RESULT
+        + getRUStandaloneExpectedResult()
+        + getRUExpectedResult();
   }
 
   @Test
@@ -67,20 +124,36 @@ public class MonthTest extends DesugaredLibraryTestBase {
     if (parameters.isCfRuntime()) {
       testForJvm(parameters)
           .addInnerClasses(MonthTest.class)
-          .run(parameters.getRuntime(), MonthTest.Main.class)
-          .assertSuccessWithOutput(getExpectedResult(parameters));
+          .run(parameters.getRuntime(), Main.class)
+          .assertSuccessWithOutput(getExpectedResult());
       return;
     }
     testForDesugaredLibrary(parameters, libraryDesugaringSpecification, compilationSpecification)
         .addInnerClasses(getClass())
         .addKeepMainRule(Main.class)
         .run(parameters.getRuntime(), Main.class)
-        .assertSuccessWithOutput(getExpectedResult(parameters));
+        .assertSuccessWithOutput(getExpectedResult());
   }
 
   static class Main {
+
     public static void main(String[] args) {
       System.out.println(Month.APRIL.getDisplayName(TextStyle.FULL_STANDALONE, Locale.UK));
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.SHORT_STANDALONE, Locale.UK));
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.NARROW_STANDALONE, Locale.UK));
+
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.FULL, Locale.UK));
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.SHORT, Locale.UK));
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.NARROW, Locale.UK));
+
+      Locale ru = new Locale("ru");
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.FULL_STANDALONE, ru));
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.SHORT_STANDALONE, ru));
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.NARROW_STANDALONE, ru));
+
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.FULL, ru));
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.SHORT, ru));
+      System.out.println(Month.APRIL.getDisplayName(TextStyle.NARROW, ru));
     }
   }
 }
